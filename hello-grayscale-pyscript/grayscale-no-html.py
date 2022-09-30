3929c94e6667c0dd7674b6d7148ce0484814f20e
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-09-30 15:37:33 -0700

    Finalize python version of grayscale

---
+++ hello-grayscale-pyscript/grayscale-no-html.py
@@ -0,0 +1,35 @@
+import string
+from PIL import Image 
+import sys 
+
+
+def grayscale(img: str) -> None:
+    """Converts an image (passed as a string to the file) into grayscale."""
+    try:
+        # import image
+        input_image = Image.open(img)
+    except:
+        print("File " + img + " could not be opened.")
+        return 
+
+    # extract pixel map 
+    pixel_map = input_image.load()
+    # get dimenstions
+    width, height = input_image.size
+    # make it grayscale!
+    for i in range(width):
+        for j in range(height):
+            r, g, b = input_image.getpixel((i, j))
+
+            avg = int((0.299*r + 0.587*g + 0.114*b))
+            pixel_map[i, j] = (avg, avg, avg)
+    # svae new image
+    input_image.save(img+".-grayscale.png", format = "png")
+
+
+imgs = sys.argv[1:]
+for img in imgs:
+    grayscale(str(img))
+
+
+
