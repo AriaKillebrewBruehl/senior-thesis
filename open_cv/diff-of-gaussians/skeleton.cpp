30ed8b313b0809e9d777b03ee93c86673adfee60
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-18 09:09:33 -0700

    Read in image

--- open_cv/diff-of-gaussians/skeleton.cpp
+++ open_cv/diff-of-gaussians/skeleton.cpp
@@ -1,11 +1,33 @@
 #include "skeleton.hpp"
 
+cv::Mat skeleton(std::string path, cv::Mat img) {
+    // read in image
+    cv::Mat image;
+    if (img.empty() && path == "") {
+        throw "Must pass in either file path, opencv image, or both";
+        return image;
+    }
+    if (img.empty() && path != "") {
+        // read image
+        image = cv::imread(path, 1);
+        if (image.empty()) {
+            throw "Not a valid image file.";
+            return image;
+        }   
+    } else if (!img.empty()) {
+        image = img;
+    }
+     
+
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
          std::cerr << "Must pass in image to run DoG on." << std::endl;
     } else {
         for (int i = 1; i < argc; i++) {
-            DoG(argv[i]);
+            cv::Mat img;
+            skeleton(argv[i],img );
         }
     }
 }
