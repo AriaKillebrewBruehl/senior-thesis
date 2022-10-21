781a8af3b990a93e34bf0fef2d49e21b5f1b26ec
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-20 17:25:34 -0700

    Change to optimized

--- open_cv/diff-of-gaussians/skeleton.cpp
+++ open_cv/diff-of-gaussians/skeleton.cpp
@@ -20,19 +20,41 @@ cv::Mat skeleton(std::string path, cv::Mat img) {
     // convert to binary 
     cv::threshold(image, image, 127, 255, cv::THRESH_BINARY);
 
+    // skeleton image and temp image 
+    cv::Mat skel(image.size(), CV_8UC1, cv::Scalar(0));
+    cv::Mat temp(image.size(), CV_8UC1);
+    cv::Mat eroded;
+
+    // use a 3 x 3 cross shape for 4-connexity 
+    //   []
+    // [][][]
+    //   []
+    cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
+    
+    bool done;		
+    do
+    {
+        cv::erode(image, eroded, element);
+        cv::dilate(eroded, temp, element); // temp = open(image)
+        cv::subtract(image, temp, temp);
+        cv::bitwise_or(skel, temp, skel);
+        eroded.copyTo(image);
+        
+        done = (cv::countNonZero(image) == 0);
+    } while (!done);
+
 
     // save image
     if (path == "") {
-        int rand = std::rand() % 100;
-        path = "./" + std::to_string(rand) + ".png";
+        srand (time(NULL));
+        int rand = std::rand() % 1000;
+        path = "../images/" + std::to_string(rand) + ".png";
     }
     std::string file_type = path.substr(path.length()-4, 4);
     std::string output_file = path + "-skel" + file_type;
-    cv::imwrite(output_file, image);
+    cv::imwrite(output_file, skel);
 
-    return image;
-     
-
+    return skel;
 }
 
 int main(int argc, char** argv) {
@@ -40,8 +62,8 @@ int main(int argc, char** argv) {
          std::cerr << "Must pass in image to run DoG on." << std::endl;
     } else {
         for (int i = 1; i < argc; i++) {
-            cv::Mat img;
-            skeleton(argv[i],img );
+            cv::Mat image;
+            skeleton(argv[i],image );
         }
     }
 }
