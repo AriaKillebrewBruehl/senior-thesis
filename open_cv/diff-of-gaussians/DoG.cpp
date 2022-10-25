778a673ec50d8489fe5e23c5503f79685302e063
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-24 19:05:50 -0700

    Clean up code and get her working!

--- open_cv/diff-of-gaussians/DoG.cpp
+++ open_cv/diff-of-gaussians/DoG.cpp
@@ -1,4 +1,4 @@
-# include "DoG.hpp"
+#include "DoG.hpp"
 
 cv::Mat DoG(std::string path, cv::Mat img) {
     // read in image
@@ -18,16 +18,22 @@ cv::Mat DoG(std::string path, cv::Mat img) {
         image = img;
     } 
 
+    // convert image to grayscale 
+    cv::Mat gray_mat(image.size(), CV_8U);
+    cv::cvtColor(image, gray_mat, cv::COLOR_BGR2GRAY);
+
     cv::Mat low_sigma;
     cv::Mat high_sigma;
 
-    cv::GaussianBlur(image, low_sigma, cv::Size(3, 3), 0, 0);
-    cv::GaussianBlur(image, high_sigma, cv::Size(9, 9), 0, 0);
+    // cv::GaussianBlur(gray_mat, low_sigma, cv::Size(3, 3), 0, 0);
+    // cv::GaussianBlur(gray_mat, high_sigma, cv::Size(9, 9), 0, 0);
+    cv::GaussianBlur(gray_mat, low_sigma, cv::Size(0, 0), 1.0, 0);
+    cv::GaussianBlur(gray_mat, high_sigma, cv::Size(0, 0), 1.5, 0);
+   
    
     cv::Mat DoG = low_sigma - high_sigma;
     // convert to bi-level image
     cv::Mat image_th;
-
     double thresh = cv::threshold(DoG, image_th, 10, 255, cv::THRESH_BINARY);
 
     // save image
