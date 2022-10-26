f18a843b1cae7312383df4c5c00c2363cfc3fb45
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-25 20:08:49 -0700

    Comment out main

--- open_cv/diff-of-gaussians/DoG.cpp
+++ open_cv/diff-of-gaussians/DoG.cpp
@@ -50,14 +50,14 @@ cv::Mat DoG(std::string path, cv::Mat img) {
 }
 
 
-// int main(int argc, char** argv) {
-//     if (argc < 2) {
-//          std::cerr << "Must pass in image to run DoG on." << std::endl;
-//     } else {
-//         for (int i = 1; i < argc; i++) {
-//             cv::Mat image;
-//             image = cv::imread(argv[i], 1);
-//             DoG("", image);
-//         }
-//     }
-// }
+int main(int argc, char** argv) {
+    if (argc < 2) {
+         std::cerr << "Must pass in image to run DoG on." << std::endl;
+    } else {
+        for (int i = 1; i < argc; i++) {
+            cv::Mat image;
+            // image = cv::imread(argv[i], 1);
+            DoG(argv[i], image);
+        }
+    }
+}
