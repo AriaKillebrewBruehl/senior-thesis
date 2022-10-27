e5b809a1190284ebb203fc03010957fa8771ccac
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-27 10:48:05 -0700

    Remove morph-open function

--- open_cv/diff-of-gaussians/extract-edges.cpp
+++ open_cv/diff-of-gaussians/extract-edges.cpp
@@ -1,32 +1,15 @@
 #include "extract-edges.hpp"
 
-
-cv::Mat morph_open(std::string file_path, cv::Mat image) {
-    cv::Mat opened;
-    cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2, 2));
-
-    cv::morphologyEx(image, opened, cv::MORPH_OPEN, element);
-    // save image
-    std::string file_type = file_path.substr(file_path.length()-4, 4);
-    std::string output_file_morphed = file_path + "-morphed-"+ file_type;
-    cv::imwrite(output_file_morphed, opened);
-    
-    return opened;
-}
-
-
-
-
 cv::Mat extractEdges(std::string path, cv::Mat img) {
-     // read in image
+    // read in image
     cv::Mat image;
     if (img.empty() && path == "") {
         throw "Must pass in either file path, opencv image, or both";
         return image;
     }
     if (img.empty() && path != "") {
-        // read image
-        image = cv::imread(path, 0);
+        // read image as grayscale
+        image = cv::imread(path, 1);
         if (image.empty()) {
             throw "Not a valid image file.";
             return image;
@@ -34,19 +17,24 @@ cv::Mat extractEdges(std::string path, cv::Mat img) {
     } else if (!img.empty()) {
         image = img;
     }
-
     // run DoG
     cv::Mat dog = DoG(path, image);
+    std::string file_type = path.substr(path.length()-4, 4);
+    std::string output_file = path + "-extracted-DOG" + file_type;
+    cv::imwrite(output_file, dog);
 
     // extract edges via threshold
-    cv::Mat extracted = threshold("", dog, 100);
+    cv::Mat extracted = threshold("", dog, 200);
+    file_type = path.substr(path.length()-4, 4);
+    output_file = path + "-extracted-thresh" + file_type;
+    cv::imwrite(output_file, extracted);
 
     // morphological operations
     cv::Mat morphed;
     cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2, 2));
-
+    std::cout << type2str(extracted.type()) << std::endl;
+    extracted.convertTo(extracted, CV_8UC1);
     cv::morphologyEx(extracted, morphed, cv::MORPH_OPEN, element);
-
 
     // save image
     if (path == "") {
@@ -54,20 +42,20 @@ cv::Mat extractEdges(std::string path, cv::Mat img) {
         int rand = std::rand() % 1000;
         path = "../images/" + std::to_string(rand) + ".png";
     }
-    std::string file_type = path.substr(path.length()-4, 4);
-    std::string output_file = path + "-thresh" + file_type;
-    cv::imwrite(output_file, image);
+    file_type = path.substr(path.length()-4, 4);
+    output_file = path + "-extracted" + file_type;
+    cv::imwrite(output_file, morphed);
 
     return image;
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-         std::cerr << "Must pass in image to run DoG on." << std::endl;
+         std::cerr << "Must pass in image to extract edges of." << std::endl;
     } else {
         for (int i = 1; i < argc; i++) {
             cv::Mat image;
-            threshold(argv[i], image, 50); 
+            extractEdges(argv[i], image); 
         }
     }
 }
