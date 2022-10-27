49c55b942b18f11500e8f05653f91e5451426b02
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-27 10:49:57 -0700

    Rename folders

--- open_cv/isophotes/isophotes.cpp
+++ open_cv/isophotes/isophotes.cpp
@@ -1,7 +1,27 @@
 #include "isophotes.hpp"
 
+inline uchar reduceVal(const uchar val)
+{
+    if (val < 192) return uchar(val / 64.0 + 0.5) * 64;
+    return 255;
+}
+
+void processColors(cv::Mat& img)
+{
+    uchar* pixelPtr = img.data;
+    for (int i = 0; i < img.rows; i++)
+    {
+        for (int j = 0; j < img.cols; j++)
+        {
+            const int pi = i*img.cols*3 + j*3;
+            pixelPtr[pi + 0] = reduceVal(pixelPtr[pi + 0]); // B
+            pixelPtr[pi + 1] = reduceVal(pixelPtr[pi + 1]); // G
+            pixelPtr[pi + 2] = reduceVal(pixelPtr[pi + 2]); // R
+        }
+    }
+}
+
 cv::Mat getIsophotes(std::string path, cv::Mat img) {
-    cv::Mat image;
     // read in image
     cv::Mat image;
     if (img.empty() && path == "") {
@@ -10,7 +30,7 @@ cv::Mat getIsophotes(std::string path, cv::Mat img) {
     }
     if (img.empty() && path != "") {
         // read in image unchanged
-        image = cv::imread(path, 0);
+        image = cv::imread(path);
         if (image.empty()) {
             throw "Not a valid image file.";
             return image;
@@ -28,6 +48,13 @@ cv::Mat getIsophotes(std::string path, cv::Mat img) {
         // }
     }
 
+    // convert image to CIE L*a*b
+    std::cout << type2str(image.type()) << std::endl;
+    std::cout << image.channels() << std::endl; 
+    std::cout << image.depth() << std::endl;
+    // cv::Mat cie(image.size(), image.type());
+    cv::cvtColor(image, image, cv::COLOR_BGR2Lab);
+
     // save image
     if (path == "") {
         srand (time(NULL));
@@ -35,7 +62,30 @@ cv::Mat getIsophotes(std::string path, cv::Mat img) {
         path = "../images/" + std::to_string(rand) + ".png";
     }
     std::string file_type = path.substr(path.length()-4, 4);
-    std::string output_file = path + "-iso" + file_type;
+    std::string output_file = path + "-cie" + file_type;
+    cv::imwrite(output_file, image);
+
+    processColors(image);
+
+
+    // save image
+    if (path == "") {
+        srand (time(NULL));
+        int rand = std::rand() % 1000;
+        path = "../images/" + std::to_string(rand) + ".png";
+    }
+    output_file = path + "-reduced" + file_type;
     cv::imwrite(output_file, image);
     return image;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+         std::cerr << "Must pass in image to run DoG on." << std::endl;
+    } else {
+        for (int i = 1; i < argc; i++) {
+            cv::Mat image;
+            getIsophotes(argv[i],image);
+        }
+    }
 }
--- open_cv/isophotes/isophotes.hpp
+++ open_cv/isophotes/isophotes.hpp
@@ -5,3 +5,6 @@
 #include <unordered_map>
 #include <random>
 #include <opencv2/opencv.hpp>
+#include "../diff-of-gaussians/type2str.cpp"
+
+cv::Mat getIsophotes(std::string path, cv::Mat img);
