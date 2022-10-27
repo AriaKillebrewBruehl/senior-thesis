9ccdd376b7cb51670a6ea061396cda31eff360e2
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-27 10:47:46 -0700

    Change kernels

--- open_cv/diff-of-gaussians/DoG.cpp
+++ open_cv/diff-of-gaussians/DoG.cpp
@@ -22,13 +22,17 @@ cv::Mat DoG(std::string path, cv::Mat img) {
     cv::Mat gray_mat(image.size(), CV_8U);
     cv::cvtColor(image, gray_mat, cv::COLOR_BGR2GRAY);
 
+    std::string file_type = path.substr(path.length()-4, 4);
+    std::string output_file = path + "-DOG-grayscale" + file_type;
+    cv::imwrite(output_file, gray_mat);
+
     cv::Mat low_sigma;
     cv::Mat high_sigma;
 
-    // cv::GaussianBlur(gray_mat, low_sigma, cv::Size(3, 3), 0, 0);
-    // cv::GaussianBlur(gray_mat, high_sigma, cv::Size(9, 9), 0, 0);
-    cv::GaussianBlur(gray_mat, low_sigma, cv::Size(0, 0), 1.0, 0);
-    cv::GaussianBlur(gray_mat, high_sigma, cv::Size(0, 0), 1.5, 0);
+    cv::GaussianBlur(gray_mat, low_sigma, cv::Size(3, 3), 0, 0);
+    cv::GaussianBlur(gray_mat, high_sigma, cv::Size(9, 9), 0, 0);
+    // cv::GaussianBlur(gray_mat, low_sigma, cv::Size(0, 0), 1.0, 0);
+    // cv::GaussianBlur(gray_mat, high_sigma, cv::Size(0, 0), 1.5, 0);
    
    
     cv::Mat DoG = low_sigma - high_sigma;
@@ -42,22 +46,22 @@ cv::Mat DoG(std::string path, cv::Mat img) {
         int rand = std::rand() % 1000;
         path = "../images/" + std::to_string(rand) + ".png";
     }
-    std::string file_type = path.substr(path.length()-4, 4);
-    std::string output_file = path + "-DoG" + file_type;
+    file_type = path.substr(path.length()-4, 4);
+    output_file = path + "-DoG" + file_type;
     cv::imwrite(output_file, image_th);
 
     return image_th;
 }
 
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-         std::cerr << "Must pass in image to run DoG on." << std::endl;
-    } else {
-        for (int i = 1; i < argc; i++) {
-            cv::Mat image;
-            // image = cv::imread(argv[i], 1);
-            DoG(argv[i], image);
-        }
-    }
-}
+// int main(int argc, char** argv) {
+//     if (argc < 2) {
+//          std::cerr << "Must pass in image to run DoG on." << std::endl;
+//     } else {
+//         for (int i = 1; i < argc; i++) {
+//             cv::Mat image;
+//             // image = cv::imread(argv[i], 1);
+//             DoG(argv[i], image);
+//         }
+//     }
+// }
