895c10738d98b44d365be16bd95ecd68cb6036ea
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-31 17:35:11 -0700

    Combine read and save to one file

--- open_cv/isophotes/isophotes.hpp
+++ open_cv/isophotes/isophotes.hpp
@@ -6,6 +6,6 @@
 #include <random>
 #include <opencv2/opencv.hpp>
 #include "../edge-detection/type2str.cpp"
-#include "save.cpp"
+#include "read-save.hpp"
 
 cv::Mat getIsophotes(std::string path, cv::Mat img, bool saving);
--- open_cv/isophotes/read-save.cpp
+++ open_cv/isophotes/read-save.cpp
@@ -1,5 +1,4 @@
-#include <string>
-#include <opencv2/opencv.hpp>
+#include "read-save.hpp"
 
 cv::Mat read(std::string path, cv::Mat img) {
     cv::Mat image;
@@ -18,4 +17,17 @@ cv::Mat read(std::string path, cv::Mat img) {
     }
 
     return image;
+}
+
+void save(cv::Mat image, std::string path, std::string suffix) {
+    if (path == "") {
+        srand (time(NULL));
+        int rand = std::rand() % 1000;
+        path = "../images/" + std::to_string(rand) + ".png";
+    }
+    std::string file_type = path.substr(path.length()-4, 4);
+    std::string output_file = path + suffix + file_type;
+    cv::imwrite(output_file, image);
+
+    return;
 }
