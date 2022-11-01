cc8d33b84c384780c87d33659240b1bef5949dab
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-10-31 18:39:11 -0700

    Add read-save

--- open_cv/edge-detection/threshold-edges.hpp
+++ open_cv/edge-detection/threshold-edges.hpp
@@ -1,13 +1,10 @@
-#pragma once
-
 #include <iostream>
 #include <string>
 #include <random>
 #include <unordered_map>
 #include <opencv2/opencv.hpp>
 #include "skeleton.hpp"
-#include "save.cpp"
-#include "read.cpp"
+#include "read-save.hpp"
 
 cv::Mat threshold(std::string path, cv::Mat img, int threshold, bool saving);
 bool meetsThreshold(cv::Mat img, int threshold);
