011948157462f37294fed04ca8309dc87fb836c1
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-01 16:59:19 -0700

    Rename folders

--- open_cv/jfa/jfa.hpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <unordered_map>
-#include <opencv2/opencv.hpp>
-#include "read-save.hpp"
-
-using pixel_type = std::pair<int, int>;
-using seed_map = std::unordered_map<pixel_type, pixel_type, hash_pair>;  
