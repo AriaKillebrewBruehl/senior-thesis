5757090f183687ce2669897bdbcc6302262d26e1
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-02 20:47:22 -0700

    Add type2str

--- open_cv/distance-map/distance-map.hpp
+++ open_cv/distance-map/distance-map.hpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <unordered_map>
 #include <opencv2/opencv.hpp>
+#include "type2str.cpp"
 #include "read-save.hpp"
 #include "jfa.hpp"
 
-cv::Mat distanceMap(std::string path, cv::Mat img, bool saving);
+cv::Mat distanceMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos, cv::Mat imgIsos, bool saving);
