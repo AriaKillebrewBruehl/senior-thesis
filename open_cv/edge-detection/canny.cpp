e005a682275f7b4bb24dc713aa26a710711bd160
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-05 16:25:17 -0700

    Implement canny algorithm

--- open_cv/edge-detection/canny.cpp
+++ open_cv/edge-detection/canny.cpp
@@ -4,9 +4,23 @@ cv::Mat cannyFilter(std::string path, cv::Mat img, bool saving) {
     cv::Mat image;
     image = read(path, img);
 
+    cv::Mat dest(image.rows, image.cols, CV_8UC3);
+    cv::Canny(image, dest, 100, 200);
 
     if (saving) {
-        save(image, path, "-canny");
+        save(dest, path, "-canny");
     }
-    return image;
+    return dest;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+         std::cerr << "Must pass in image to run DoG on." << std::endl;
+    } else {
+        for (int i = 1; i < argc; i++) {
+            cv::Mat image;
+            // image = cv::imread(argv[i], 1);
+            cannyFilter(argv[i], image, true);
+        }
+    }
 }
