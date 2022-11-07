57adf6e38a81a2cdac2f02a8d451b68266a1f4af
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-07 12:01:42 -0800

    Add canny and fix type2str

--- open_cv/edge-detection/extract-edges.hpp
+++ open_cv/edge-detection/extract-edges.hpp
@@ -2,10 +2,10 @@
 #include <string>
 #include <unordered_map>
 #include <opencv2/opencv.hpp>
-
+#include "canny.hpp"
 #include "DoG.hpp"
 #include "threshold-edges.hpp"
-#include "type2str.cpp"
+#include "type2str.hpp"
 #include "read-save.hpp"
 
 cv::Mat extractEdges(std::string path, cv::Mat img, bool saving);
