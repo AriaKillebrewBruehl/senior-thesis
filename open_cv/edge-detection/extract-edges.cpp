c4e31542aea8aea1c1498fb4bbd7878cc7c685ad
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-07 12:01:31 -0800

    Use read and check that image is correct type

--- open_cv/edge-detection/extract-edges.cpp
+++ open_cv/edge-detection/extract-edges.cpp
@@ -4,45 +4,31 @@ cv::Mat extractEdges(std::string path, cv::Mat img, bool saving) {
     // read in image
     cv::Mat image;
     image = read(path, img);
-    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
-    // if (img.empty() && path == "") {
-    //     throw "Must pass in either file path, opencv image, or both";
-    //     return image;
-    // }
-    // if (img.empty() && path != "") {
-    //     // read image as grayscale
-    //     image = cv::imread(path, 1);
-    //     if (image.empty()) {
-    //         throw "Not a valid image file.";
-    //         return image;
-    //     }
-    // } else if (!img.empty()) {
-    //     image = img;
-    // }
+    if (image.type() != 0) {
+        throw "Image must be single chanel grayscale image";
+    }
 
     // run DoG
-    cv::Mat dog = DoG(path, image, false);
+    // cv::Mat dog = DoG(path, image, false);
+    cv::Mat canny = cannyFilter(path, image, false);
 
-    cv::Mat morphed;
+    // cv::Mat morphed;
     cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2, 2));
-    dog.convertTo(dog, CV_8UC1);
-    cv::morphologyEx(dog, morphed, cv::MORPH_CLOSE, element);
-    cv::Mat morphed3;
-    morphed.convertTo(morphed, CV_8UC1);
-    cv::morphologyEx(morphed, morphed3, cv::MORPH_CLOSE, element);
-    cv::Mat morphed4;
-    morphed3.convertTo(morphed3, CV_8UC1);
-    cv::morphologyEx(morphed3, morphed4, cv::MORPH_CLOSE, element);
-    std::string file_type = path.substr(path.length()-4, 4);
-    std::string output_file = path + "-extracted-pre-thresh-morph" + file_type;
-    cv::imwrite(output_file,  morphed3);
+    // canny.convertTo(canny, CV_8UC1);
+    // cv::morphologyEx(canny, morphed, cv::MORPH_CLOSE, element);
+    // cv::Mat morphed3;
+    // morphed.convertTo(morphed, CV_8UC1);
+    // cv::morphologyEx(morphed, morphed3, cv::MORPH_CLOSE, element);
+    // cv::Mat morphed4;
+    // morphed3.convertTo(morphed3, CV_8UC1);
+    // cv::morphologyEx(morphed3, morphed4, cv::MORPH_CLOSE, element);
+    // std::string file_type = path.substr(path.length()-4, 4);
+    // std::string output_file = path + "-extracted-pre-thresh-morph" + file_type;
+    // cv::imwrite(output_file,  morphed3);
 
 
     // extract edges via threshold
-    cv::Mat extracted = threshold("", morphed, 200, false);
-    file_type = path.substr(path.length()-4, 4);
-    output_file = path + "-extracted-thresh" + file_type;
-    cv::imwrite(output_file, extracted);
+    cv::Mat extracted = threshold("", canny, 50, false);
 
     // morphological operations
     cv::Mat morphed2;
@@ -55,7 +41,7 @@ cv::Mat extractEdges(std::string path, cv::Mat img, bool saving) {
         save(morphed2, path, "-extracted");
     }
 
-    return image;
+    return morphed2;
 }
 
 int main(int argc, char** argv) {
