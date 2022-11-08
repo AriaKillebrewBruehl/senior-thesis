0c07818ec7acb7fc7519b97d9e63a11a3d1c22d7
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-08 11:17:19 -0800

    Add distance map and function signature

--- open_cv/kitten-caboodle/caboodle.hpp
+++ open_cv/kitten-caboodle/caboodle.hpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
-
+#include "../distance-map/distance-map.hpp"
 #include "../edge-detection/extract-edges.hpp"
-#include "../isophote/extract-isophotes.hpp"
+#include "../isophotes/extract-isophotes.hpp"
 #include "read-save.hpp"
 #include "type2str.hpp"
 
+cv::Mat caboodle(std::string path, cv::Mat img, bool saving);
