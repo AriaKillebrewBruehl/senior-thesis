8d174cd33188c2e0ac1de3aafe5141c50237a2c8
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-08 15:24:02 -0800

    Add test file

---
+++ open_cv/distance-map/tests.cpp
@@ -0,0 +1,12 @@
+#include "tests.hpp"
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+         std::cerr << "Must pass in image to run DoG on." << std::endl;
+    } else {
+        for (int i = 1; i < argc; i+=2) {
+            cv::Mat image;
+            distanceMap(argv[i],image, argv[i + 1], image, true);
+        }
+    }
+}
---
+++ open_cv/distance-map/tests.hpp
@@ -0,0 +1,1 @@
+#include "distance-map.hpp"
