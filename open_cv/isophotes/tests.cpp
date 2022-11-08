19ab0368a4b16d67b98d43558049f1d62e185c64
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-08 15:02:49 -0800

    Add file for running tests

---
+++ open_cv/isophotes/tests.cpp
@@ -0,0 +1,12 @@
+#include "tests.hpp"
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+         std::cerr << "Must pass in image to run DoG on." << std::endl;
+    } else {
+        for (int i = 1; i < argc; i++) {
+            cv::Mat image;
+            extractIsophotes(argv[i],image, true);
+        }
+    }
+}
