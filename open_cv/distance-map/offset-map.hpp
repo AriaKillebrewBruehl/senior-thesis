51e2d6b05631f839d50959d81adc905c30ae6b9d
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-14 18:04:17 -0800

    Update headers

--- open_cv/distance-map/offset-map.hpp
+++ open_cv/distance-map/offset-map.hpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <cmath>
 #include <opencv2/opencv.hpp>
 #include "distance-map.hpp"
+
+cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, std::string pathPriorities, cv::Mat imgPriorities, bool saving);
