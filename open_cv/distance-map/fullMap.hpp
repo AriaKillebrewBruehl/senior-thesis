67e52c0537c91713c9678a5f38dd8cc6642a7c99
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-14 18:35:26 -0800

    Add function signature

--- open_cv/distance-map/fullMap.hpp
+++ open_cv/distance-map/fullMap.hpp
@@ -2,3 +2,5 @@
 #include "distance-map.hpp"
 #include "offset-map.hpp"
 #include "combine.hpp"
+
+cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos, cv::Mat imgIsos, bool saving);
