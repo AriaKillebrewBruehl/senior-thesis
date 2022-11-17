12ba741a9df0988f20b5abf2d0e5d11528863422
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-17 10:41:36 -0800

    Add type handling

--- open_cv/distance-map/offset-map.cpp
+++ open_cv/distance-map/offset-map.cpp
@@ -4,41 +4,45 @@ cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, std::string pathPrior
     // read images and resize
     cv::Mat dists;
     dists = read(pathDists, imgDists);
+    try {
+        if (dists.empty()) {
+            throw 0;
+        }
+    } catch (int i) {
+        std::cout << "ERROR: Could not read in image in offsetMap." << std::endl;
+        return dists;
+    }
+    if (dists.type() != 0) {
+        std::cout << "ERROR: Input image must be single chanel in offsetMap." << std::endl;
+        return dists;
+    }
     cv::Mat priorities;
     priorities = read(pathPriorities, imgPriorities);
-
-    cv::Mat offsetMap = cv::Mat::zeros(dists.rows, dists.cols, CV_8UC1);
-    float scalar = 1.5;
-    for (int i = 0; i < offsetMap.rows; i++) {
-        for (int j = 0; j < offsetMap.cols; j++) { 
-            int dist = int(dists.at<uchar>(i, j));
-            if (sin(dist * scalar) >= 0) {
-                offsetMap.at<uchar>(i, j) = uchar(255);
-            }
-            // std::cout << int(offsetMap.at<uchar>(i, j)) << std::endl;
+    try {
+        if (priorities.empty()) {
+            throw 0;
         }
+    } catch (int i) {
+        std::cout << "ERROR: Could not read in image in offsetMap." << std::endl;
+        return priorities;
+    }
+    if (priorities.type() != 0) {
+        std::cout << "ERROR: Input image must be single chanel in offsetMap." << std::endl;
+        return dists;
     }
 
-
-    // int l = 6;
-    // int w0 = 1; // width of the offset line
-    // int id0 = 0;
-    // int Rmin = ((l - w0) * id0) - int(w0/2);
-    // int Rmax = ((l - w0) * id0) + int(w0/2);
+    cv::Mat offsetMap = cv::Mat::zeros(dists.rows, dists.cols, CV_8UC1);
+    // float scalar = 1.5;
     // for (int i = 0; i < offsetMap.rows; i++) {
-    //     for (int j = 0; j < offsetMap.cols; j++) {
-    //         int df = int(dists.at<cv::Vec3b>(i, j)[0]);
-    //         std::cout << "df: " << df << std::endl;
-    //         int pf = int(priorities.at<cv::Vec3b>(i, j)[0]);
-    //         std::cout << "pf: " << pf << std::endl;
-    //         std::cout << "df * pf: " << df * pf << std::endl;
-    //         if (df * pf >= Rmin && df * pf <= Rmax) {
-    //             offsetMap.at<cv::Vec3b>(i, j)[0] = 255;
-    //             offsetMap.at<cv::Vec3b>(i, j)[1] = 255;
-    //             offsetMap.at<cv::Vec3b>(i, j)[2] = 255;
+    //     for (int j = 0; j < offsetMap.cols; j++) { 
+    //         int dist = int(dists.at<uchar>(i, j));
+    //         if (sin(dist * scalar) >= 0) {
+    //             offsetMap.at<uchar>(i, j) = uchar(255);
     //         }
     //     }
     // }
+
+
 
     if (saving) {
         save(offsetMap, pathDists, "-o-map");
