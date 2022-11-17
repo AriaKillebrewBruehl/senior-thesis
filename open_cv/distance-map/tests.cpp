0c763084721da9430365f775e041f504fbe43d8e
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-17 10:26:59 -0800

    Change function called

--- open_cv/distance-map/tests.cpp
+++ open_cv/distance-map/tests.cpp
@@ -6,7 +6,8 @@ int main(int argc, char** argv) {
     } else {
         for (int i = 1; i < argc; i+=2) {
             cv::Mat image;
-            fullMap(argv[i],image, argv[i + 1], image, true);
+            jmp_flood(argv[i], image, true);
+            // fullMap(argv[i],image, argv[i + 1], image, true);
             // offsetMap(argv[i],image, argv[i + 1], image, true);
             // distanceMap(argv[i],image, argv[i + 1], image, true);
         }
