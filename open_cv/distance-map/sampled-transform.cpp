fc4e8e785af2b09288851d9552ed934832ebe396
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-28 09:46:58 -0800

    Change to using input and output matricies

--- open_cv/distance-map/sampled-transform.cpp
+++ open_cv/distance-map/sampled-transform.cpp
@@ -1,18 +1,57 @@
 #include "sampled-transform.hpp"
 
-cv::Mat DT(std::string path, cv::Mat img,bool saving) {
-    cv::Mat image;
-    image = read(path, img);
+cv::Mat DTOneDim(cv::Mat arr, std::function<int(int)> f) {
     try {
-        if (image.empty()) {
+        if (arr.empty()) {
             throw 0;
         }
     } catch (int i) {
-        std::cout << "ERROR: Could not read in image in distanceMap." << std::endl;
-        return image;
+        std::cout << "ERROR: Empty matrix in DTOneDim." << std::endl;
+        return arr;
+    }
+    try {
+        if (arr.rows != 1 && arr.cols != 1) {
+            throw 0;
+        }
+    } catch (int i) {
+        std::cout << "ERROR: Input matrix in DTOneDim has dimensions " << arr.rows << " x " << arr.cols << ". Must be a single row or single column matrix." << std::endl;
+        return arr;
     }
 
-    if (saving) {
-        save(image, path, "-dt");
+    cv::Mat Df = cv::Mat::zeros(arr.rows, arr.cols, CV_8UC1); // output matrix 
+    int k = 0; // index of right-most parabola in lower envelope 
+    std::vector<int, int> v;
+    std::vector<int, int> z;
+
+    v[0] = 0;       // locations of parabolas in lower envelope 
+    z[0] = INT_MIN; // locations of boundaries between parabolas 
+    z[1] = INT_MAX;
+
+    for (int i = 0; i < arr.rows; i++) {
+        for (int j = 0; j < arr.cols; j++) {
+            int q = int(arr.at<uchar>(i, j));
+            double s = ((f(q) + std::pow(q,2)) - (f(v[k]) + std::pow(v[k], 2))) / (2 * q - 2 * v[k]);
+            if (s <= z[k]) {
+                k--;
+                continue;
+            } else {
+                k++;
+                v[k] = q;
+                z[k] = s;
+                z[k+1] = INT_MAX;
+            }
+        }
     }
+
+    k = 0;
+    for (int i = 0; i < arr.rows; i++) {
+        for (int j = 0; j < arr.cols; j++) {
+            int q = int(arr.at<uchar>(i, j));
+            while (z[k+1] < q) {
+                k++;
+            }
+            Df.at<uchar>(i, j) = uchar((q-v[k]) * 2 + f(v[k]));
+        }
+    }
+    return Df;
 }
