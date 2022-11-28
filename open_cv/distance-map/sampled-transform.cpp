84d836e14ec1486545efb75b9c4963b6b606847c
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-27 21:17:01 -0800

    Reading and saving

--- open_cv/distance-map/sampled-transform.cpp
+++ open_cv/distance-map/sampled-transform.cpp
@@ -1,5 +1,18 @@
 #include "sampled-transform.hpp"
 
 cv::Mat DT(std::string path, cv::Mat img,bool saving) {
+    cv::Mat image;
+    image = read(path, img);
+    try {
+        if (image.empty()) {
+            throw 0;
+        }
+    } catch (int i) {
+        std::cout << "ERROR: Could not read in image in distanceMap." << std::endl;
+        return image;
+    }
 
+    if (saving) {
+        save(image, path, "-dt");
+    }
 }
--- open_cv/distance-map/sampled-transform.hpp
+++ open_cv/distance-map/sampled-transform.hpp
@@ -2,5 +2,6 @@
 #include <string>
 #include <math.h>
 #include <opencv2/opencv.hpp>
+#include "read-save.hpp"
 
 cv::Mat DT(std::string path, cv::Mat img,bool saving);
