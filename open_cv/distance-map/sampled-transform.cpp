d8641720e8bffbc3003caea925d9fc2a112b4d7c
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-30 18:15:00 -0800

    Change function of look at positions, not values

--- open_cv/distance-map/sampled-transform.cpp
+++ open_cv/distance-map/sampled-transform.cpp
@@ -1,9 +1,28 @@
 #include "sampled-transform.hpp"
 
 // indicator function for membership in a set of seed pixels
-int f(int p) {
+int f(cv::Mat arr, int p) {
+    try {
+        if (arr.type() != 0) {
+            throw arr.type();
+        }
+    } catch (int t) {
+        std::cout << "ERROR: matrix passed to f was of type " << t << ".\n";
+        std::cout << "Function f only accepts matrices of type 0." << std::endl;
+        return INT_MAX;
+    }
+    try {
+        if (arr.cols != 1) {
+            throw arr.cols;
+        }
+    } catch (int i) {
+        std::cout << "ERROR: Input matrix in DTOneDim has " << i << " columns. " << "Must be a single column matrix." << std::endl;
+        return INT_MAX;
+    } 
+
+    int value = arr.at<int>(p, 0);
     // std::cout << "f(" << p << ") = ";
-    if (p == 255) {
+    if (value == 255) {
         // std::cout << "255" << std::endl;
         return 0;
     }
@@ -30,48 +49,57 @@ cv::Mat DTOneDim(cv::Mat arr, std::function<int(int)> f) {
         return arr;
     }
 
+    bool rotated = false;
+    // multiple columns in one row 
+    if (arr.cols != 1) {
+        // rotate matrix 90 degrees to create a single column matrix 
+        cv::rotate(arr, arr, cv::ROTATE_90_CLOCKWISE);
+    }
+    assert(arr.cols == 1);
+
+
     cv::Mat Df = cv::Mat::zeros(arr.rows, arr.cols, CV_8UC1); // output matrix 
     int k = 0; // index of right-most parabola in lower envelope 
     std::vector<int> v{0};
     std::vector<int> z{INT_MIN, INT_MAX}; // locations of boundaries between parabolas 
 
+    // single column matrix so just loop over the rows
     for (int i = 0; i < arr.rows; i++) {
-        for (int j = 0; j < arr.cols; j++) {
-            std::cout << k << std::endl;
-            int q = arr.at<int>(i, j);
-            // std::cout << "(" << i << ", " << j << ") = " << q << ", f(q) = " << f(q) << std::endl;
-            
-            bool done = false;
-            int s;
-            while (!done) {
-                int r = arr.at<int>(0, v[k]);
-                s = ((f(q) + (q*q)) - (f(r) + (r*r))) / (2 * q - 2 * r);
-                // std::cout << "k: " << k << " s: " << s << " z[k]: " << z[k] << std::endl;
-                std::cout << "s: " << s << std::endl;
-                if (s > z[k]) {
-                    done = true;
-                }
-                k--;
+        
+        std::cout << k << std::endl;
+        int value = arr.at<int>(i, 0);
+        // std::cout << "(" << i << ", " << j << ") = " << q << ", f(q) = " << f(q) << std::endl;
+        
+        bool done = false;
+        int s;
+        while (!done) {
+            s = ((f(arr, i) + (i*i)) - (f(arr, v[k]) + (v[k]*v[k]))) / (2 * i - 2 * v[k]);
+            // std::cout << "k: " << k << " s: " << s << " z[k]: " << z[k] << std::endl;
+            std::cout << "s: " << s << std::endl;
+            if (s > z[k]) {
+                done = true;
+            }
+            k--;
             }
         
             k++;
-            v[k] = q;
+            v[k] = i;
             z[k] = s;
             z[k+1] = INT_MAX;
-        }
     }
 
 
     k = 0;
     for (int i = 0; i < arr.rows; i++) {
-        for (int j = 0; j < arr.cols; j++) {
-            int q = arr.at<int>(i, j);
-            while (z[k+1] < q) {
-                k++;
-            }
-            int a = (q-v[k]);
-            Df.at<int>(i, j) = int(a * a + f(v[k]));
+        while (z[k+1] < i) {
+            k++;
         }
+        int a = (i-v[k]);
+        Df.at<int>(i, 0) = int(a * a + f(v[k]));
+    }
+
+    if (rotated) {
+        cv::rotate(Df, Df, cv::ROTATE_90_COUNTERCLOCKWISE);
     }
 
     std::cout << "DF: " << Df << std::endl;
