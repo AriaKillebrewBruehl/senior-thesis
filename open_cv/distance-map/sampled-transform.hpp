d32dfc1208318fdf9ae0ba59fa39316b9b344955
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-11-30 18:15:57 -0800

    Update signatures

--- open_cv/distance-map/sampled-transform.hpp
+++ open_cv/distance-map/sampled-transform.hpp
@@ -4,4 +4,11 @@
 #include <opencv2/opencv.hpp>
 #include "read-save.hpp"
 
-cv::Mat DT(std::string path, cv::Mat img,bool saving);
+// indicator function for membership in a set of seed pixels
+int f(cv::Mat arr, int p);
+
+cv::Mat DTOneDim(cv::Mat arr, std::function<int(int)> f);
+
+cv::Mat test(cv::Mat arr, std::string path);
+
+cv::Mat sample(cv::Mat img, std::string path, bool saving);
