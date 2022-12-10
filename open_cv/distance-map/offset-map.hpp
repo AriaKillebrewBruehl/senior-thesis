f6adcaba6bd1b541d9cbafb2968294584012c157
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-12-10 15:03:14 -0800

    Add distance map 2

--- open_cv/distance-map/offset-map.hpp
+++ open_cv/distance-map/offset-map.hpp
@@ -2,6 +2,6 @@
 #include <string>
 #include <math.h>
 #include <opencv2/opencv.hpp>
-#include "distance-map.hpp"
+#include "distance-map2.hpp"
 
 cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, bool saving);
