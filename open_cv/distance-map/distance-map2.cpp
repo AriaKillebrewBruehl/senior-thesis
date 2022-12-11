40939c0c33b6a5a5303bfa0ba49f4f45ed8d7aa4
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-12-11 12:58:03 -0800

    Remove priority buffer and change types

--- open_cv/distance-map/distance-map2.cpp
+++ open_cv/distance-map/distance-map2.cpp
@@ -16,6 +16,7 @@ distMap distanceMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIso
     if (edges.type() == 0) {
         cv::cvtColor(edges, edges, cv::COLOR_GRAY2BGR);
     }
+
     cv::Mat isos;
     isos = read(pathIsos, imgIsos);
      try {
@@ -31,39 +32,31 @@ distMap distanceMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIso
         cv::cvtColor(isos, isos, cv::COLOR_GRAY2BGR);
     }
     
-    cv::Mat edgedists = sample(edges, "", false);
-    cv::Mat isosdists = sample(isos, "", false);
+    cv::Mat edgedists = sample(edges, pathEdges, true);
+    cv::Mat isosdists = sample(isos, pathIsos, true);
 
-    cv::Mat distances = cv::Mat::zeros(edges.rows, edges.cols, CV_8UC1);
-    cv::Mat priorityBuffer = cv::Mat::zeros(edges.rows, edges.cols, CV_8UC1);
-
+    cv::Mat distances = cv::Mat::zeros(edges.rows, edges.cols, CV_32SC1);
+   
     int edge_weight = 1;
     int isos_weight = 2;
+
     for (int i = 0; i < distances.rows; i++) {
         for (int j = 0; j < distances.cols; j++) {
-    
-            float edge_dist = edgedists.at<float32_t>(i, j) / edge_weight;
-            float isos_dist = isosdists.at<float32_t>(i, j) / isos_weight;
+            float edge_dist = edgedists.at<int32_t>(i, j) / edge_weight;
+            float isos_dist = isosdists.at<int32_t>(i, j) / isos_weight;
             if (isos_dist < edge_dist) {
-                isos_dist = int(isos_dist) % 255;
-                distances.at<uchar>(i, j) = uchar(isos_dist);
-                priorityBuffer.at<uchar>(i, j) = uchar(isos_weight);
+                distances.at<int32_t>(i, j) = int32_t(isos_dist);
             } else {
-                edge_dist = int(edge_dist) % 255;
-                distances.at<uchar>(i, j) = uchar(edge_dist);
-                priorityBuffer.at<uchar>(i, j) = uchar(edge_weight);
-            }
+                distances.at<int32_t>(i, j) = int32_t(edge_dist);
         }
     }
     // save image
     if (saving) {
-        save(distances, pathEdges, "-dists-0");
-        save(priorityBuffer, pathEdges, "-priorities-0");
+        save(distances, pathEdges, "-dists-1");
     }
 
     distMap dists;
     dists.distances = distances;
-    dists.priorityBuffer = priorityBuffer;
 
     return dists;
 }
