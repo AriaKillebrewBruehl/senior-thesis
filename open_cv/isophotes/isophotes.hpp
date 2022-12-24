1bd620c21086fd0f653c9f651cffe7ef12c271ad
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-12-24 14:23:31 -0800

    Include global files

--- open_cv/isophotes/isophotes.hpp
+++ open_cv/isophotes/isophotes.hpp
@@ -5,7 +5,7 @@
 #include <unordered_map>
 #include <random>
 #include <opencv2/opencv.hpp>
-#include "type2str.hpp"
-#include "read-save.hpp"
+#include "../boiler-plate/type2str.hpp"
+#include "../boiler-plate/read-save.hpp"
 
 cv::Mat getIsophotes(std::string path, cv::Mat img, bool saving);
--- open_cv/kitten-caboodle/caboodle.hpp
+++ open_cv/kitten-caboodle/caboodle.hpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
-#include "../distance-map/distance-map.hpp"
+#include "../distance-map/distance-map2.hpp"
 #include "../edge-detection/extract-edges.hpp"
 #include "../edge-detection/gray-scale.hpp"
 #include "../isophotes/extract-isophotes.hpp"
-#include "read-save.hpp"
-#include "type2str.hpp"
+#include "../boiler-plate/read-save.hpp"
+#include "../boiler-plate/type2str.hpp"
 
 cv::Mat caboodle(std::string path, cv::Mat img, bool saving);
