9f87c1513ed42794ee51ffcdb3dbb587b34de9ff
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-12-24 14:00:43 -0800

    Move no longer used files

--- open_cv/distance-map/read-save.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-#include <opencv2/opencv.hpp>
-#include "type2str.hpp"
-
-cv::Mat read(std::string path, cv::Mat img);
-void save(cv::Mat image, std::string path, std::string suffix);
