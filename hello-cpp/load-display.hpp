d02896cc17cc2b248fa98b6d8bf8488ee8d36612
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-12-24 13:47:12 -0800

    Move folders

--- hello-cpp/load-display.hpp
+++
@@ -1,4 +0,0 @@
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <iostream>
--- hello-js/hello-image.py
+++
@@ -1,9 +0,0 @@
-import imageio.v3 as iio
-
-# read in an image
-im = iio.imread("images/butterfly-line.jpg")
-# get it's shape 
-# height, width, channel (3 = 3 color bits)
-print(im.shape)
-
-
