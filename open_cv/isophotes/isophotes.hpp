b7aa7c788962ab20e31d330f06166ea90ad1dc42
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-03 14:54:14 -0800

    Min heap comparison operator

--- open_cv/isophotes/isophotes.hpp
+++ open_cv/isophotes/isophotes.hpp
@@ -8,4 +8,18 @@
 #include "../boiler-plate/type2str.hpp"
 #include "../boiler-plate/read-save.hpp"
 
+typedef std::pair<int, int> color_freq;
+
+struct comp {
+    constexpr bool operator()(
+    std::pair<int, int> const& a,
+    std::pair<int, int> const& b)
+    const noexcept
+    {
+        // min heap
+        return a.second < b.second;
+    }
+};
+
 cv::Mat getIsophotes(std::string path, cv::Mat img, bool saving);
+void processColors(cv::Mat& img);
