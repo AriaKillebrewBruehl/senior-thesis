fc9858a24ca56bd01f9768918029e61d2691ddde
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-05 14:08:02 -0800

    Update error message

--- open_cv/isophotes/tests.cpp
+++ open_cv/isophotes/tests.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-         std::cerr << "Must pass in image to run DoG on." << std::endl;
+         std::cerr << "Must pass in image to run isophotes tests on." << std::endl;
     } else {
         for (int i = 1; i < argc; i++) {
             cv::Mat image;
