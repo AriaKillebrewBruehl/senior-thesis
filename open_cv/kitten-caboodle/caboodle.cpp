03593b7b4a18af849a83503a1009928a4ba191bf
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-06 14:57:47 -0800

    Change saving method and add background color check and inversion

--- open_cv/kitten-caboodle/caboodle.cpp
+++ open_cv/kitten-caboodle/caboodle.cpp
@@ -33,27 +33,63 @@ cv::Mat caboodle(std::string path, cv::Mat img, bool saving) {
     }
 
     // step 1: extract the edges of the image
-    cv::Mat edges = extractEdges(path, gray, true);
+    cv::Mat edges = extractEdges(path, gray, 1000, true);
+    assert(edges.type() == 0);
     std::cout << "extracted edges from image" << std::endl;
+    
+    // get components
+    cv::Mat labels;
+    cv::Mat stats;
+    cv::Mat centroids;
+    int numComps =  cv::connectedComponentsWithStats(edges, labels, stats, centroids);
+
+    int i;
+    int j;
+    for (i = 0; i < labels.rows; i++) {
+        for (j = 0; j < labels.cols; j++) {
+            if (labels.at<int>(i, j) == 0) {
+                break;
+            }
+        }
+    }
+
+    // background is black so invert
+    if (edges.at<int>(i,j) == 0) {
+        cv::bitwise_not(edges, edges);
+    }
+    
 
     // step 2: extract the isophotes of the image
-    cv::Mat isophotes = extractIsophotes(path, image, true);
+    cv::Mat isophotes = extractIsophotes(path, image, 150, 5, true);
+    assert(isophotes.type() == 0);
     std::cout << "extracted isophotes from image" << std::endl;
 
-    // step 3: offset map
-    cv::Mat map = fullMap(path, edges, path, isophotes, false);
-    std::cout << "extracted distance map from image" << std::endl;
+    // get components
+    numComps =  cv::connectedComponentsWithStats(isophotes, labels, stats, centroids);
 
-    if (saving) {
-        save(map, path, "-caboodled");
+    for (i = 0; i < labels.rows; i++) {
+        for (j = 0; j < labels.cols; j++) {
+            if (labels.at<int>(i, j) == 0) {
+                break;
+            }
+        }
     }
+
+    // background is black so invert
+    if (isophotes.at<int>(i,j) == 0) {
+        cv::bitwise_not(isophotes, isophotes);
+    }
+    
+    // step 3: offset map
+    cv::Mat map = fullMap(path, edges, path, isophotes, saving);
+    std::cout << "extracted distance map from image" << std::endl;
 
     return map;
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-         std::cerr << "Must pass in image to extract edges of." << std::endl;
+         std::cerr << "Must pass in image to caboodle." << std::endl;
     } else {
         for (int i = 1; i < argc; i++) {
             cv::Mat image;
