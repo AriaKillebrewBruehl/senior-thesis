cc0ac47eefb1752ed85a5ac0980e9f86daef3487
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-18 14:38:11 -0800

    Implement inversion for background correction

--- open_cv/distance-map/fullMap.cpp
+++ open_cv/distance-map/fullMap.cpp
@@ -12,13 +12,37 @@ cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos, c
         std::cout << "ERROR: Could not read in image in distanceMap." << std::endl;
         return edges;
     }
+
+    assert(edges.depth() == 0);
+
+    // get components
+    cv::Mat labels;
+    cv::Mat stats;
+    cv::Mat centroids;
+    int numComps =  cv::connectedComponentsWithStats(edges, labels, stats, centroids);
+
+    int i;
+    int j;
+    for (i = 0; i < labels.rows; i++) {
+        for (j = 0; j < labels.cols; j++) {
+            if (labels.at<int>(i, j) == 0) {
+                break;
+            }
+        }
+    }
+
+    // background is black so invert
+    if (edges.at<int>(i,j) == 0) {
+        cv::bitwise_not(edges, edges);
+    }
+
     if (edges.type() == 16) {
         cv::cvtColor(edges, edges, cv::COLOR_GRAY2BGR);
     } 
     if (edges.type() != 4) {
         edges.convertTo(edges, CV_32SC1);
     }
-
+    
     cv::Mat isos;
     isos = read(pathIsos, imgIsos);
      try {
@@ -29,6 +53,25 @@ cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos, c
         std::cout << "ERROR: Could not read in image in distanceMap." << std::endl;
         return edges;
     }
+
+    assert(isos.depth() == 0);
+
+    // get components
+    numComps =  cv::connectedComponentsWithStats(isos, labels, stats, centroids);
+
+    for (i = 0; i < labels.rows; i++) {
+        for (j = 0; j < labels.cols; j++) {
+            if (labels.at<int>(i, j) == 0) {
+                break;
+            }
+        }
+    }
+
+    // background is black so invert
+    if (isos.at<int>(i,j) == 0) {
+        cv::bitwise_not(isos, isos);
+    }
+
     if (isos.type() == 16) {
         cv::cvtColor(isos, isos, cv::COLOR_GRAY2BGR);
     } 
