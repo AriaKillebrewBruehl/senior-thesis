b90160621ca0485d2ce0364e283e19b183964fb1
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-26 15:50:38 -0800

    Call place dots

--- open_cv/dots/tests.cpp
+++ open_cv/dots/tests.cpp
@@ -4,8 +4,9 @@ int main(int argc, char** argv) {
     if (argc < 2) {
          std::cerr << "Must pass in image to run DoG on." << std::endl;
     } else {
-        for (int i = 1; i < argc; i+=2) {
-           
+        for (int i = 1; i < argc; i++) {
+            cv::Mat image;
+            placeDots(argv[i], image, 6, true);
         }
     }
 }
