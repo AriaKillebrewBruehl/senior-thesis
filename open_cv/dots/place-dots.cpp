7a51e5737792027711d68583267a199e9204e5d2
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-26 15:50:31 -0800

    Add dot placement

--- open_cv/dots/place-dots.cpp
+++ open_cv/dots/place-dots.cpp
@@ -4,18 +4,27 @@ cv::Mat placeDots(std::string path, cv::Mat offsetMap, int d, bool saving) {
     cv::Mat image;
     image = read(path, offsetMap);
 
+    // TODO type conversion
+
     // blank map for seed pixels
     cv::Mat seeds = cv::Mat::zeros(image.rows, image.cols, CV_8UC1);
+    srand(time(0));
     for (int i = 0; i < image.rows; i++) {
         for (int j = 0; j < image.cols; j++) {
-
+            // feature lines are white
+            if (image.at<uchar>(i, j) != 255) {
+                double r = ((double)rand()) / RAND_MAX;
+                if (r <=  1 / double(d*d)) {
+                    seeds.at<uchar>(i, j) = 255;
+                }
+            }
         }
     }
 
 
     if (saving) {
-        save(image, path, "-dots");
+        save(seeds, path, "-dots");
     }
 
-    return image;
+    return seeds;
 }
