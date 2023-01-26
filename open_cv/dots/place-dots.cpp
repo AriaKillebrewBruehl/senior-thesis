90df0f2698dca9ffbd1b074c3cdfd760900e17e7
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-26 14:36:22 -0800

    Add distance parameter

--- open_cv/dots/place-dots.cpp
+++ open_cv/dots/place-dots.cpp
@@ -1,8 +1,16 @@
 #include "place-dots.hpp"
 
-cv::Mat placeDots(std::string path, cv::Mat offsetMap, bool saving) {
+cv::Mat placeDots(std::string path, cv::Mat offsetMap, int d, bool saving) {
     cv::Mat image;
     image = read(path, offsetMap);
+
+    // blank map for seed pixels
+    cv::Mat seeds = cv::Mat::zeros(image.rows, image.cols, CV_8UC1);
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+
+        }
+    }
 
 
     if (saving) {
--- open_cv/dots/place-dots.hpp
+++ open_cv/dots/place-dots.hpp
@@ -2,4 +2,4 @@
 #include "../boiler-plate/read-save.hpp"
 
 
-cv::Mat placeDots(std::string path, cv::Mat offsetMap, bool saving);
+cv::Mat placeDots(std::string path, cv::Mat offsetMap, int d, bool saving);
