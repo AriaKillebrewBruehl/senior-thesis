ba8d52dd2b0eee02c23681d93f7dcb1b47b02f25
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-27 11:52:55 -0800

    Remove seed information

--- open_cv/distance-map/sampled-transform-2.hpp
+++ open_cv/distance-map/sampled-transform-2.hpp
@@ -4,29 +4,6 @@
 #include <opencv2/opencv.hpp>
 #include "../boiler-plate/read-save.hpp"
 
-struct hash_pair {
-    template <class T1, class T2>
-    size_t operator()(const std::pair<T1, T2>& p) const {
-        auto hash1 = std::hash<T1>{}(p.first);
-        auto hash2 = std::hash<T2>{}(p.second);
- 
-        if (hash1 != hash2) {
-            return hash1 ^ hash2;             
-        }
-        // If hash1 == hash2, their XOR is zero.
-        return hash1;
-    }
-};
-
-using pixel_type = std::pair<int, int>;
-using seed_map = std::unordered_map<pixel_type, pixel_type, hash_pair>; 
-
-const pixel_type undef(-1, -1);
-
-struct sampled_pair {
-    cv::Mat sampled;
-    cv::Mat seeds;
-};
 
 // indicator function for membership in a set of seed pixels
 int32_t func(int32_t x);
