f66a176ef94308694119d43eec03ee28c7ddea68
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-01-28 15:39:42 -0800

    Call sample_seeds

--- open_cv/distance-map/tests.cpp
+++ open_cv/distance-map/tests.cpp
@@ -4,18 +4,9 @@ int main(int argc, char** argv) {
     if (argc < 2) {
          std::cerr << "Must pass in image to run DoG on." << std::endl;
     } else {
-        for (int i = 1; i < argc; i+=2) {
+        for (int i = 1; i < argc; i+=1) {
             cv::Mat image;
-            // image = read(argv[i], image);
-            // cv::bitwise_not(image, image);
-            // save(image, argv[i], "-white");
-            // sample(image, argv[i], true);
-
-            // jmp_flood2(argv[i], image, true);
-
-            fullMap(argv[i],image, argv[i + 1], image, true);
-            // offsetMap(argv[i],image, true);
-            // distanceMap(argv[i],image, argv[i + 1], image, true);
+            sample_seeds(image, argv[i], true, false);
         }
     }
 }
