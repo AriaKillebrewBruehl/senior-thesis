4a9de31052955086204f42cedf763d5bafed6579
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-03 11:19:28 -0800

    Formatting

--- open_cv/boiler-plate/invert.cpp
+++ open_cv/boiler-plate/invert.cpp
@@ -1,29 +1,25 @@
 #include "invert.hpp"
 
-cv::Mat invert(cv::Mat arr)
-{
+cv::Mat invert(cv::Mat arr) {
     // get components
     cv::Mat labels;
     cv::Mat stats;
     cv::Mat centroids;
-    int numComps = cv::connectedComponentsWithStats(arr, labels, stats, centroids);
+    int numComps =
+        cv::connectedComponentsWithStats(arr, labels, stats, centroids);
 
     int i;
     int j;
-    for (i = 0; i < labels.rows; i++)
-    {
-        for (j = 0; j < labels.cols; j++)
-        {
-            if (labels.at<int>(i, j) == 0)
-            {
+    for (i = 0; i < labels.rows; i++) {
+        for (j = 0; j < labels.cols; j++) {
+            if (labels.at<int>(i, j) == 0) {
                 break;
             }
         }
     }
 
     // background is black so invert
-    if (arr.at<int>(i, j) == 0)
-    {
+    if (arr.at<int>(i, j) == 0) {
         cv::bitwise_not(arr, arr);
     }
 
