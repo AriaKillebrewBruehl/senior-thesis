5603415889159461b526f2bce3a9b6df542ab89b
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-03 11:26:38 -0800

    Update type checking

--- open_cv/isophotes/extract-isophotes.cpp
+++ open_cv/isophotes/extract-isophotes.cpp
@@ -1,35 +1,19 @@
 #include "extract-isophotes.hpp"
 
-cv::Mat extractIsophotes(std::string path, cv::Mat img, int edgethresh, int isothresh, bool saving) {
+cv::Mat extractIsophotes(std::string path, cv::Mat img, int edgethresh,
+                         int isothresh, bool saving) {
     cv::Mat image;
     image = read(path, img);
-    try {
-        if (image.empty()) {
-            throw 0;
-        }
-    } catch (int i) {
-        std::cout << "ERROR: Could not read in image in extractIsophotes." << std::endl;
-        return image;
-    }
+    assert(!image.empty());
 
-    try {
-        if (image.type() == 0) {
-            cv::cvtColor(image, image, cv::COLOR_GRAY2BGR);
-        }
-        if (image.type() != 16) {
-            throw image.type();
-        }
-    } catch (int t) {
-        std::cout << "Input image in extractIsophotes must be of type 8UC3." << std::endl;
-        std::cout << "Provided image was of type " << type2str(t) << "." << std::endl;
-        cv::Mat empty;
-        return empty;
+    if (image.type() == 0) {
+        image.convertTo(image, 0);
     }
 
     // get isophote image
     cv::Mat isos = getIsophotes(path, image, isothresh, false);
     assert(isos.type() == 0);
-    
+
     // extract edges from isophote image
     cv::Mat isosExtracted = extractEdges(path, isos, edgethresh, false);
     assert(isos.type() == 0);
--- open_cv/isophotes/isophotes.cpp
+++ open_cv/isophotes/isophotes.cpp
@@ -1,7 +1,6 @@
 #include "isophotes.hpp"
 
-inline uchar reduceVal(const uchar val)
-{
+inline uchar reduceVal(const uchar val) {
     if (val < 192) return uchar(val / 64.0 + 0.5) * 64;
     return 255;
 }
@@ -16,7 +15,7 @@ cv::Mat processColors(cv::Mat& img) {
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
             uchar L = reduceVal(img.at<cv::Vec3b>(i, j)[0]);
-            gs.at<uchar>(i,j) = (L);
+            gs.at<uchar>(i, j) = (L);
             colors[L]++;
         }
     }
@@ -26,38 +25,24 @@ cv::Mat processColors(cv::Mat& img) {
 cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
     // read in image
     cv::Mat image = read(path, img);
-    try {
-        if (image.empty()) {
-            throw 0;
-        }
-    } catch (int i) {
-        std::cout << "ERROR: Could not read in image in getIsophotes." << std::endl;
-        return image;
+    assert(!image.empty());
+
+    if (image.type() != 16) {
+        image.convertTo(image, 16);
     }
 
-    try {
-        if (image.type() != 16) {
-            throw image.type();
-        }
-    } catch (int t) {
-        std::cout << "ERROR: Input image in getIsophotes must be of type 8UC3." << std::endl;
-        std::cout << "ERROR: Provided image was of type " << type2str(t) << "." << std::endl;
-        cv::Mat empty;
-        return empty;
-    }
-    
     int MAX_KERNEL_LENGTH = 15;
     cv::Mat src = image;
-    // bilateral filter 
-    for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 ) {
+    // bilateral filter
+    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2) {
         cv::Mat dest;
-        cv::bilateralFilter (src, dest, i, i*2, i/2 );
+        cv::bilateralFilter(src, dest, i, i * 2, i / 2);
         src = dest;
-    }  
+    }
 
     // convert image to CIE L*a*b
     cv::cvtColor(src, src, cv::COLOR_RGB2Lab);
-    
+
     // luminance quantization and create color frequency map
     cv::Mat processed = processColors(src);
 
@@ -66,24 +51,27 @@ cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
         heap.push(i);
     }
     // take the top 1/thresh of colors
-    int s = (heap.size() <= thresh) ? 1 : std::ceil(float(heap.size()) / float(thresh));
-   
+    int s = (heap.size() <= thresh)
+                ? 1
+                : std::ceil(float(heap.size()) / float(thresh));
+
     uchar t;
     for (int i = 0; i < s; i++) {
         t = heap.top().first;
         heap.pop();
     }
-    
+
     // set all pixels >= t (lighter than t) to white
     for (int i = 0; i < processed.rows; i++) {
         for (int j = 0; j < processed.cols; j++) {
-            processed.at<uchar>(i, j) = processed.at<uchar>(i, j) >= t ? uchar(255) : uchar(0);
+            processed.at<uchar>(i, j) =
+                processed.at<uchar>(i, j) >= t ? uchar(255) : uchar(0);
         }
     }
 
     if (saving) {
         save(processed, path, "-isos");
     }
-    
+
     return processed;
 }
