02a6b2a440bc722d63433b1360b97d73fa898e5a
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-04 19:46:54 -0800

    Alternate with and without constraints

--- open_cv/dots/dots.cpp
+++ open_cv/dots/dots.cpp
@@ -8,4 +8,26 @@ cv::Mat dots(std::string path, cv::Mat img, bool saving) {
 
     // step 0: generate seeds
     cv::Mat seeds = placeDots("", image, 6, false);
+
+    int t1 = 10;
+    int t2 = 30;
+    cv::Mat adjusted;
+    while (t1 || t2) {
+        // step 1: get distance map
+        cv::Mat3i dist = sample_seeds(seeds, "", false, true);
+        // step 2: adjust seeds
+        if (t1) {
+            // no offset lane constraints
+            adjusted = adjust("", image, "", dist, false, false);
+            t1--;
+        } else {
+            // alternate offset lane constraints
+            adjusted = adjust("", image, "", dist, false, t2 % 2);
+            t2--;
+        }
+    }
+
+    if (saving) {
+        save(adjusted, path, "-adjusted");
+    }
 }
