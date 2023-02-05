fe2586e1e008f7ced8922b241a9e0cad5d6e929e
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-04 18:45:59 -0800

    Add parameter for offset constraints

--- open_cv/dots/adjust-cells.cpp
+++ open_cv/dots/adjust-cells.cpp
@@ -1,6 +1,7 @@
 #include "adjust-cells.hpp"
 
 seed_map generate_map(cv::Mat image) {
+    assert(!image.empty());
     assert(image.channels() == 3);
     seed_map map{};
 
@@ -21,9 +22,12 @@ seed_map generate_map(cv::Mat image) {
 }
 
 cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
-               std::string path_seeds, cv::Mat img_seeds, bool saving) {
+               std::string path_seeds, cv::Mat img_seeds, bool saving,
+               bool offset) {
     cv::Mat offsets = read(path_offset, img_offset);
+    assert(!offsets.empty());
     cv::Mat seeds = read(path_seeds, img_seeds);
+    assert(!seeds.empty());
 
     // TODO type checking
     // given a distance map
@@ -31,21 +35,28 @@ cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
 
     for (auto const &pair : map) {
         pixel_type center = pair.first;
-        int32_t x_sum;
-        int32_t y_sum;
+        int32_t x_sum = 0;
+        int32_t y_sum = 0;
         int32_t w = 1;
+        int32_t ro = 0;
         for (auto const &p : pair.second) {
             int32_t x = p.first;
             int32_t y = p.second;
-            // if p is part of an offset line don't include it
-            if (offsets.at<int32_t>(y, x) == 255) {
-                continue;
+            if (offset) {
+                // if p is part of an offset line don't include it
+                if (offsets.at<int32_t>(y, x) == 255) {
+                    // wi = min{D(xi)/Dw,1}
+                    // what is D(xi)
+                    continue;
+                }
+
+                // how do i tell if there is an offset line
+                // in between parts of pixels
             }
 
-            // how do i tell if there is an offset line
-            // in between parts of pixels
-            x_sum += p.first * w;
-            y_sum += p.second * w;
+            x_sum += x * w;
+            y_sum += x * w;
+            ro += w;
         }
     }
 }
