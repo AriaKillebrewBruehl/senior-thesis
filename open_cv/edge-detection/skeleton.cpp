a2cc3e15ddbc018ab350b6dd3bd0d47570702507
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-05 22:17:43 -0800

    Format

--- open_cv/edge-detection/skeleton.cpp
+++ open_cv/edge-detection/skeleton.cpp
@@ -18,43 +18,45 @@ cv::Mat skeleton(std::string path, cv::Mat img, bool saving) {
             throw image.type();
         }
     } catch (int t) {
-        std::cout << "ERROR: Input image to skeleton must be of type 8UC1." << std::endl;
-        std::cout << "ERROR: Provided image was of type " << type2str(t) << "." << std::endl;
+        std::cout << "ERROR: Input image to skeleton must be of type 8UC1."
+                  << std::endl;
+        std::cout << "ERROR: Provided image was of type " << type2str(t) << "."
+                  << std::endl;
         cv::Mat empty;
         return empty;
     }
-    
-    // convert to binary 
+
+    // convert to binary
     cv::threshold(image, image, 127, 255, cv::THRESH_BINARY);
 
-    // skeleton image and temp image 
+    // skeleton image and temp image
     cv::Mat copy;
     image.copyTo(copy);
     cv::Mat skel(image.size(), CV_8UC1, cv::Scalar(0));
     cv::Mat temp(image.size(), CV_8UC1);
     cv::Mat eroded;
 
-    // use a 3 x 3 cross shape for 4-connexity 
+    // use a 3 x 3 cross shape for 4-connexity
     //   []
     // [][][]
     //   []
-    cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
-    
+    cv::Mat element =
+        cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
+
     // if the image is not totally white
     if (cv::countNonZero(copy) != (copy.rows * copy.cols)) {
-        bool done;	
-        do
-        {
+        bool done;
+        do {
             cv::erode(copy, eroded, element);
-            cv::dilate(eroded, temp, element); // temp = open(image)
+            cv::dilate(eroded, temp, element);  // temp = open(image)
             cv::subtract(copy, temp, temp);
             cv::bitwise_or(skel, temp, skel);
             eroded.copyTo(copy);
-            
+
             done = (cv::countNonZero(copy) == 0);
         } while (!done);
     } else {
-        skel = copy; 
+        skel = copy;
     }
 
     if (saving) {
