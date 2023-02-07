ab48e48085873ce27a3583c699cc4058ba9130cf
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-06 19:11:19 -0800

    Avoid seed pixels in map

--- open_cv/dots/adjust-cells.cpp
+++ open_cv/dots/adjust-cells.cpp
@@ -7,15 +7,16 @@ seed_map generate_map(cv::Mat image) {
 
     for (int i = 0; i < image.rows; i++) {
         for (int j = 0; j < image.cols; j++) {
-            pixel_type seed;
-            seed.first = image.at<cv::Vec3i>(i, j)[1];
-            seed.second = image.at<cv::Vec3i>(i, j)[2];
+            if (image.at<int32_t>(i, j) != 255) {
+                pixel_type seed;
+                seed.first = image.at<cv::Vec3i>(i, j)[1];
+                seed.second = image.at<cv::Vec3i>(i, j)[2];
 
-            pixel_type current;
-            current.first = i;
-            current.second = j;
-
-            map[seed].push_back(current);
+                pixel_type current;
+                current.first = i;
+                current.second = j;
+                map[seed].push_back(current);
+            }
         }
     }
     return map;
@@ -39,11 +40,13 @@ cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
 
     // given a distance map
     seed_map map = generate_map(seeds);
+
     cv::Mat adjusted = cv::Mat::zeros(seeds.rows, seeds.cols, CV_32SC1);
+    int i = 0;
     for (auto const &pair : map) {
         int32_t x_sum = 0;
         int32_t y_sum = 0;
-        int32_t w = 1;
+        float w = 1.0;
         int32_t ro = 0;
         for (auto const &p : pair.second) {
             int32_t x = p.first;
@@ -52,12 +55,15 @@ cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
                 // if p is part of an offset line don't include it
                 if (offsets.at<int32_t>(y, x) == 255) {
                     // wi = min{D(xi)/Dw,1}
-                    // what is D(xi)
+                    // what is D(xi) the value of the pixel in the distance map
                     continue;
                 }
 
                 // how do i tell if there is an offset line
                 // in between parts of pixels
+                // label all the connected regions that are between the offset
+                // lines if the region of the centroid is different from the
+                // region of the current pixel, don't include it
             }
             x_sum += x * w;
             y_sum += x * w;
@@ -66,12 +72,12 @@ cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
         int32_t final_x = x_sum / ro;
         int32_t final_y = y_sum / ro;
         adjusted.at<int32_t>(final_x, final_y) = int32_t(255);
+        i++;
         // std::cout << "prev center: " << pair.first.first << ", "
         //           << pair.first.second << std::endl;
         // std::cout << "new center:  " << final_x << ", " << final_y <<
         // std::endl; std::cout << std::endl;
     }
-
     if (saving) {
         save(adjusted, path_offset, "-new-dots");
     }
