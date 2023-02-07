7fc57d7058b1c941b65bd4fd68b78c9f62c0673d
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-06 19:11:37 -0800

    Implement type checking

--- open_cv/dots/place-dots.cpp
+++ open_cv/dots/place-dots.cpp
@@ -4,18 +4,24 @@ cv::Mat placeDots(std::string path, cv::Mat offsetMap, int d, bool saving) {
     cv::Mat image;
     image = read(path, offsetMap);
 
-    // TODO type conversion
+    assert(!image.empty());
+    if (image.type() != CV_32SC1) {
+        if (image.channels() != 1) {
+            cv::cvtColor(image, image, cv::COLOR_RGB2GRAY);
+        }
+        image.convertTo(image, CV_32SC1);
+    }
 
     // blank map for seed pixels
-    cv::Mat seeds = cv::Mat::zeros(image.rows, image.cols, CV_8UC1);
+    cv::Mat seeds = cv::Mat::zeros(image.rows, image.cols, CV_32SC1);
     srand(time(0));
     for (int i = 0; i < image.rows; i++) {
         for (int j = 0; j < image.cols; j++) {
             // feature lines are white
-            if (image.at<uchar>(i, j) != 255) {
+            if (image.at<int32_t>(i, j) != 255) {
                 double r = ((double)rand()) / RAND_MAX;
                 if (r <= 1 / double(d * d)) {
-                    seeds.at<uchar>(i, j) = 255;
+                    seeds.at<int32_t>(i, j) = 255;
                 }
             }
         }
