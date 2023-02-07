0d2481f43168cf7625d7785d2b7b4c061a16c20b
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-07 07:53:31 -0800

    Write doc string

--- open_cv/dots/place-dots.hpp
+++ open_cv/dots/place-dots.hpp
@@ -1,4 +1,11 @@
 #include <iostream>
+
 #include "../boiler-plate/read-save.hpp"
 
+/*
+cv::Mat placeDots(std::string path, cv::Mat offsetMap, int d, bool saving)
+Given an offset map with sections labeled NOT a
+visual map or a map with lines labeled, randomly places seed pixels in black on
+a white image.
+*/
 cv::Mat placeDots(std::string path, cv::Mat offsetMap, int d, bool saving);
