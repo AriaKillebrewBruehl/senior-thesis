8859dc8e6de57df750fb341ff3b867b2c33537b4
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-06 19:51:43 -0800

    Add function description

--- open_cv/distance-map/offset-map.hpp
+++ open_cv/distance-map/offset-map.hpp
@@ -1,7 +1,18 @@
-#include <iostream>
-#include <string>
 #include <math.h>
+
+#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <string>
+
 #include "distance-map2.hpp"
 
-cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, bool saving);
+/*
+    cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, bool saving,
+                  bool lines);
+    If lines == true, returns an offset map with a white background and offset
+    lines of color corrsponding to the line's id. If lines == false, returns
+    an offset map with black offset lines and sections of color corresponding to
+    the section id.
+*/
+cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, bool saving,
+                  bool lines);
