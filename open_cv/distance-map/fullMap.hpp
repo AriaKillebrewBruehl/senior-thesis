f31a1efe960bcc4d210a7bd87425d09436858067
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-06 19:54:26 -0800

    Add function description

--- open_cv/distance-map/fullMap.hpp
+++ open_cv/distance-map/fullMap.hpp
@@ -4,5 +4,14 @@
 #include "distance-map2.hpp"
 #include "offset-map.hpp"
 
+/*
+    cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string
+pathIsos, cv::Mat imgIsos, bool saving);
+
+Given an input image of edges and an
+input image of isophotes, generates a distance map from the edges and isophotes,
+then returns an offset map with a white background and offset lines of color
+corrsponding to the line's id
+*/
 cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos,
                 cv::Mat imgIsos, bool saving);
