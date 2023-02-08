de1bfcd1a281f53412fbb2cbf904870e83f709a9
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-08 12:08:11 -0800

    Implement type checking and update variable names

--- open_cv/dots/dots.cpp
+++ open_cv/dots/dots.cpp
@@ -1,34 +1,38 @@
 #include "dots.hpp"
 
 cv::Mat dots(std::string path, cv::Mat img, bool saving) {
-    cv::Mat image = read(path, img);
-    assert(!image.empty());
+    cv::Mat offsets = read(path, img);
+    assert(!offsets.empty());
 
-    // TODO type checking
+    if (offsets.type() != 4) {
+        if (offsets.channels() != 1) {
+            cv::cvtColor(offsets, offsets, cv::COLOR_RGB2GRAY);
+        }
+        offsets.convertTo(offsets, 4);
+    }
 
     // step 0: generate seeds
-    cv::Mat seeds = placeDots("", image, 6, true);
+    cv::Mat seeds = placeDots("", offsets, 6, true);
 
-    int t1 = 10;
-    int t2 = 30;
-    cv::Mat adjusted;
+    int t1 = 1;
+    int t2 = 0;
     while (t1 || t2) {
-        // step 1: get distance map
-        cv::Mat3i dist = sample_seeds(seeds, "", false, true);
+        // // step 1: get distance map
+        cv::Mat3i dists = sample_seeds(seeds, "", false, true);
         // step 2: adjust seeds
         if (t1) {
             // no offset lane constraints
-            adjusted = adjust("", image, "", dist, false, false);
+            seeds = adjust("", offsets, "", dists, false, false);
             t1--;
         } else {
             // alternate offset lane constraints
-            adjusted = adjust("", image, "", dist, false, t2 % 2);
+            seeds = adjust("", offsets, "", dists, false, t2 % 2);
             t2--;
         }
     }
 
     if (saving) {
-        save(adjusted, path, "-adjusted");
+        save(seeds, path, "-adjusted");
     }
-    return adjusted;
+    return seeds;
 }
