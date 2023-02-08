6489d1d2aba5ababba640863a1dc1dca8ffbc7a9
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-08 12:07:51 -0800

    Update variable names, attempt checking which section a dot is in

--- open_cv/dots/adjust-cells.cpp
+++ open_cv/dots/adjust-cells.cpp
@@ -19,12 +19,21 @@ seed_map generate_map(cv::Mat image) {
             }
         }
     }
+    for (auto pair : map) {
+        std::cout << "(" << pair.first.first << ", " << pair.first.second
+                  << "): [";
+        for (auto p : pair.second) {
+            std::cout << "(" << p.first << ", " << p.second << "), ";
+        }
+        std::cout << "]" << std::endl;
+        std::cout << std::endl;
+    }
     return map;
 }
 
 cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
-               std::string path_seeds, cv::Mat img_seeds, bool saving,
-               bool offset) {
+               std::string path_dists_w_seeds, cv::Mat img_dists_w_seeds,
+               bool saving, bool offset) {
     cv::Mat offsets = read(path_offset, img_offset);
     assert(!offsets.empty());
     if (offsets.type() != 4) {
@@ -34,15 +43,17 @@ cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
         offsets.convertTo(offsets, CV_32SC1);
     }
 
-    cv::Mat seeds = read(path_seeds, img_seeds);
-    assert(!seeds.empty());
-    assert(seeds.type() == 20);
+    cv::Mat dists_w_seeds = read(path_dists_w_seeds, img_dists_w_seeds);
+    assert(!dists_w_seeds.empty());
+    assert(dists_w_seeds.type() == 20);
 
-    // given a distance map
-    seed_map map = generate_map(seeds);
+    seed_map map = generate_map(dists_w_seeds);
 
-    cv::Mat adjusted = cv::Mat::zeros(seeds.rows, seeds.cols, CV_32SC1);
+    cv::Mat adjusted = cv::Mat(dists_w_seeds.rows, dists_w_seeds.cols, CV_32SC1,
+                               cv::Scalar(255));
     for (auto const &pair : map) {
+        int32_t seed_x = pair.first.first;
+        int32_t seed_y = pair.first.second;
         float x_sum = 0;
         float y_sum = 0;
         float w = 1.0;
@@ -52,19 +63,20 @@ cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
             int32_t y = p.second;
             if (offset) {
                 // if p is part of an offset line
-                if (offsets.at<int32_t>(y, x) == 255) {
+                if (offsets.at<int32_t>(y, x) == 0) {
                     // wi = min{D(xi)/Dw,1}
-                    w = seeds.at<cv::Vec3i>(y, x)[0] >= 100
+                    w = dists_w_seeds.at<cv::Vec3i>(y, x)[0] >= 100
                             ? 1.0
-                            : float(seeds.at<cv::Vec3i>(y, x)[0]) / 100.0;
-                    continue;
+                            : float(dists_w_seeds.at<cv::Vec3i>(y, x)[0]) /
+                                  100.0;
                 }
-
-                // how do i tell if there is an offset line
-                // in between parts of pixels
-                // label all the connected regions that are between the offset
-                // lines if the region of the centroid is different from the
-                // region of the current pixel, don't include it
+                // if there is an offset line separating the current pixel and
+                // its seed
+                int seed_section = offsets.at<int32_t>(seed_y, seed_x);
+                int pixel_section = offsets.at<int32_t>(y, x);
+                if (pixel_section != seed_section) {
+                    w = 0;
+                }
             } else {
                 w = 1.0;
             }
@@ -74,7 +86,7 @@ cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
         }
         int32_t final_x = x_sum / ro;
         int32_t final_y = y_sum / ro;
-        adjusted.at<int32_t>(final_x, final_y) = int32_t(255);
+        adjusted.at<int32_t>(final_x, final_y) = 0;
     }
     if (saving) {
         save(adjusted, path_offset, "-new-dots");
