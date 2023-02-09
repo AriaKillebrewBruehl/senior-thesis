b16a081432a933229b1b4d94b0e91787676a16b4
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-08 20:20:44 -0800

    Format

--- open_cv/distance-map/tests.cpp
+++ open_cv/distance-map/tests.cpp
@@ -1,12 +1,13 @@
 #include "tests.hpp"
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     if (argc < 2) {
-         std::cerr << "Must pass in image to run DoG on." << std::endl;
+        std::cerr << "Must pass in image to run DoG on." << std::endl;
     } else {
-        for (int i = 1; i < argc; i+=1) {
+        for (int i = 1; i < argc; i += 2) {
             cv::Mat image;
-            sample_seeds(image, argv[i], true, false);
+            // distanceMap(argv[i], image, argv[i + 1], image, true);
+            sample_seeds(image, argv[i], true, true);
         }
     }
 }
