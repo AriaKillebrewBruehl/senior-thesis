0819d2928a5c887b773ac92d3998b31e185d6509
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-10 12:44:10 -0800

    Format

--- open_cv/edge-detection/extract-edges.hpp
+++ open_cv/edge-detection/extract-edges.hpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <string>
 #include <unordered_map>
-#include <opencv2/opencv.hpp>
-#include "canny.hpp"
-#include "DoG.hpp"
-#include "threshold-edges.hpp"
+
 #include "../boiler-plate/read-save.hpp"
 #include "../boiler-plate/type2str.hpp"
+#include "DoG.hpp"
+#include "canny.hpp"
+#include "threshold-edges.hpp"
 
 cv::Mat extractEdges(std::string path, cv::Mat img, int thresh, bool saving);
-
+cv::Mat processColors2(cv::Mat& img);
