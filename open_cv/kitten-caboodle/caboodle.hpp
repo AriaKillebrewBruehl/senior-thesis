092c1bc4c4aef35594ca11fc3d429167fa22b0eb
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-10 12:44:49 -0800

    Include dots

--- open_cv/kitten-caboodle/caboodle.hpp
+++ open_cv/kitten-caboodle/caboodle.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <iostream>
-#include <string>
 #include <opencv2/opencv.hpp>
+#include <string>
+
+#include "../boiler-plate/read-save.hpp"
+#include "../boiler-plate/type2str.hpp"
 #include "../distance-map/fullMap.hpp"
+#include "../dots/dots.hpp"
 #include "../edge-detection/extract-edges.hpp"
 #include "../edge-detection/gray-scale.hpp"
 #include "../isophotes/extract-isophotes.hpp"
-#include "../boiler-plate/read-save.hpp"
-#include "../boiler-plate/type2str.hpp"
 
 cv::Mat caboodle(std::string path, cv::Mat img, bool saving);
