618c0a044b4a47c4f9b2f80ca6cc553848a39626
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-11 13:56:36 -0800

    Format and include boiler plate process colors

--- open_cv/isophotes/isophotes.hpp
+++ open_cv/isophotes/isophotes.hpp
@@ -1,24 +1,22 @@
 #pragma once
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <random>
 #include <string>
 #include <unordered_map>
-#include <random>
-#include <opencv2/opencv.hpp>
-#include "../boiler-plate/type2str.hpp"
+
+#include "../boiler-plate/CIEprocess.hpp"
 #include "../boiler-plate/read-save.hpp"
+#include "../boiler-plate/type2str.hpp"
 
 typedef std::pair<uchar, int> color_freq;
 
 struct comp {
-    constexpr bool operator()(
-    std::pair<int, int> const& a,
-    std::pair<int, int> const& b)
-    const noexcept
-    {
+    constexpr bool operator()(std::pair<int, int> const& a,
+                              std::pair<int, int> const& b) const noexcept {
         return a.first < b.first;
     }
 };
 
 cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving);
-cv::Mat processColors(cv::Mat& img);
