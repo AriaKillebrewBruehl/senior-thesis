8b5b87a5b27a5f354e348158f020da75dd46ac25
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-11 12:15:16 -0800

    Fix type checking

--- open_cv/isophotes/extract-isophotes.cpp
+++ open_cv/isophotes/extract-isophotes.cpp
@@ -6,15 +6,17 @@ cv::Mat extractIsophotes(std::string path, cv::Mat img, int edgethresh,
     image = read(path, img);
     assert(!image.empty());
 
-    if (image.type() == 0) {
-        if (image.channels() != 1) {
-            cv::cvtColor(image, image, cv::COLOR_RGB2GRAY);
+    if (image.type() != 16) {
+        if (image.channels() != 3) {
+            cv::cvtColor(image, image, cv::COLOR_GRAY2RGB);
         }
-        image.convertTo(image, 0);
+        image.convertTo(image, 16);
     }
 
+    save(image, path, "-isos-input");
+
     // get isophote image
-    cv::Mat isos = getIsophotes(path, image, isothresh, false);
+    cv::Mat isos = getIsophotes(path, image, isothresh, true);
     assert(isos.type() == 0);
 
     // extract edges from isophote image
