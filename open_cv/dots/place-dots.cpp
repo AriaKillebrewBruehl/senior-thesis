ed049334660890882eacae341bd5be65797097e6
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-11 12:36:43 -0800

    Function signatures

--- open_cv/dots/place-dots.cpp
+++ open_cv/dots/place-dots.cpp
@@ -1,1 +1,3 @@
 #include "place-dots.hpp"
+
+cv::Mat placeDots(std::string path, cv::Mat img, bool saving) {}
--- open_cv/dots/place-dots.hpp
+++ open_cv/dots/place-dots.hpp
@@ -1,3 +1,5 @@
 #include <iostream>
 
 #include "../boiler-plate/read-save.hpp"
+
+cv::Mat placeDots(std::string path, cv::Mat img, bool saving);
