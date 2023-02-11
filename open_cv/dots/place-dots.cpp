f8fb127016cd4efd556f20eb312f008446f23122
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-11 12:40:19 -0800

    Type checking

--- open_cv/dots/place-dots.cpp
+++ open_cv/dots/place-dots.cpp
@@ -1,3 +1,15 @@
 #include "place-dots.hpp"
 
-cv::Mat placeDots(std::string path, cv::Mat img, bool saving) {}
+cv::Mat placeDots(std::string path, cv::Mat img, bool saving) {
+    cv::Mat image = read(path, img);
+    assert(!image.empty());
+
+    if (image.type() != 4) {
+        if (image.channels() != 1) {
+            cv::cvtColor(image, image, cv::COLOR_RGB2GRAY);
+        }
+        image.convertTo(image, 4);
+    }
+    assert(image.type() == 4);
+    return image;
+}
