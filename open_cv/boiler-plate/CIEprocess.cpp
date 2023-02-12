69790d0068a65ac5a83455984ec9f35fdec03ae2
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-12 09:20:42 -0800

    Remove comment

--- open_cv/boiler-plate/CIEprocess.cpp
+++ open_cv/boiler-plate/CIEprocess.cpp
@@ -6,7 +6,6 @@ inline uchar reduceVal(const uchar val) {
 }
 
 // return a grayscale version of the image with only the L component
-// TODO: change this to std::transform?
 cv::Mat processColors(cv::Mat img, std::unordered_map<uchar, int> *colors) {
     cv::Mat gs = cv::Mat::zeros(img.rows, img.cols, CV_8UC1);
     for (int i = 0; i < img.rows; i++) {
