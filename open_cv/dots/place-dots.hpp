e1b42c5aa5db0abf3b7490f8269c04206ad0c5e1
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-12 09:21:34 -0800

    Add invert

--- open_cv/dots/place-dots.hpp
+++ open_cv/dots/place-dots.hpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 #include "../boiler-plate/CIEprocess.hpp"
+#include "../boiler-plate/invert.hpp"
 #include "../boiler-plate/read-save.hpp"
 
 cv::Mat placeDots(std::string pathSeeds, cv::Mat imgSeeds, std::string pathOrig,
-                  cv::Mat imgOrig, bool saving);
+                  cv::Mat imagOrig, bool saving);
