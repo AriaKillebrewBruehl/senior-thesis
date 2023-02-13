544c3c6274e1e5f80e5b229b53e09c2cd0b4a91c
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-12 22:26:19 -0800

    Remove commented out code

--- open_cv/dots/place-dots.cpp
+++ open_cv/dots/place-dots.cpp
@@ -24,20 +24,7 @@ cv::Mat placeDots(std::string pathSeeds, cv::Mat imgSeeds, std::string pathOrig,
     }
     assert(image.type() == 0);
 
-    // step 1: scale up image
     int scale = 6;
-    // cv::Mat enlarged = cv::Mat(seeds.rows * scale, seeds.cols * scale,
-    // CV_8UC1,
-    //                            cv::Scalar(255));
-
-    // for (int i = 0; i < seeds.rows; i++) {
-    //     for (int j = 0; j < seeds.cols; j++) {
-    //         if (seeds.at<uchar>(i, j) == uchar(0)) {
-    //             enlarged.at<uchar>(i * scale, j * scale) = uchar(0);
-    //         }
-    //     }
-    // }
-
     cv::Mat rendered = cv::Mat(image.rows * scale, image.cols * scale, CV_8UC1,
                                cv::Scalar(255));
     float gamma = 1.2;
@@ -51,9 +38,6 @@ cv::Mat placeDots(std::string pathSeeds, cv::Mat imgSeeds, std::string pathOrig,
                     cv::circle(rendered, cv::Point2d(j * scale, i * scale),
                                int(s), cv::Scalar(0), -1);
                 }
-                // } else {
-                //     std::cout << image.at<uchar>(i, j) << std::endl;
-                // }
             }
         }
     }
