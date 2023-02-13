7afb4d2dc6b221c9c0bb2e15bdc727b535e78eaa
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-12 22:13:53 -0800

    Fix error message and looping over input

--- open_cv/dots/tests.cpp
+++ open_cv/dots/tests.cpp
@@ -2,12 +2,12 @@
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        std::cerr << "Must pass in image to run DoG on." << std::endl;
+        std::cerr << "Must pass in image to run dot placement on." << std::endl;
     } else {
-        for (int i = 1; i < argc; i++) {
+        for (int i = 1; i < argc; i += 2) {
             cv::Mat image;
-
-            dots(argv[i], image, true);
+            // dots(argv[i], image, true);
+            placeDots(argv[i], image, argv[i + 1], image, true);
         }
     }
 }
