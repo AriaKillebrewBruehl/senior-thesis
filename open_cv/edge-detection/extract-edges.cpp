edf57897f2286e272799ecc23863bd4564ae6db1
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-16 13:16:31 -0800

    Stop bilateral filtering

--- open_cv/edge-detection/extract-edges.cpp
+++ open_cv/edge-detection/extract-edges.cpp
@@ -16,25 +16,30 @@ cv::Mat extractEdges(std::string path, cv::Mat img, int thresh, bool saving) {
     }
     assert(image.type() == 16);
 
-    int MAX_KERNEL_LENGTH = 15;
-    cv::Mat src = image;
-    // bilateral filter
-    for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2) {
-        cv::Mat dest;
-        cv::bilateralFilter(src, dest, i, i * 2, i / 2);
-        src = dest;
-    }
-    // convert image to CIE L*a*b
-    cv::cvtColor(src, src, cv::COLOR_RGB2Lab);
+    // int MAX_KERNEL_LENGTH = 15;
+    // cv::Mat src = image;
+    // // bilateral filter
+    // for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2) {
+    //     cv::Mat dest;
+    //     cv::bilateralFilter(src, dest, i, i * 2, i / 2);
+    //     src = dest;
+    // }
+    // // convert image to CIE L*a*b
+    // cv::cvtColor(src, src, cv::COLOR_RGB2Lab);
 
-    // luminance quantization and create color frequency map
-    cv::Mat processed = processColors(src, nullptr);
+    // // luminance quantization and create color frequency map
+    // cv::Mat processed = processColors(src, nullptr);
 
-    assert(processed.type() == 0);
-    cv::Mat edges = cannyFilter(path, processed, false);
+    // assert(processed.type() == 0);
+    cv::Mat edges = cannyFilter(path, image, true);
 
     // extract edges via threshold
     cv::Mat extracted = threshold(path, edges, thresh, false);
+    cv::Mat inverted = invert(extracted);
+    save(inverted, path, "-extracted");
+    extracted.convertTo(extracted, CV_8UC1);
+    std::cout << type2str(extracted.type()) << std::endl;
+    cv::medianBlur(extracted, extracted, 5);
 
     // cv::Mat element =
     //     cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(1, 1));
@@ -51,10 +56,10 @@ cv::Mat extractEdges(std::string path, cv::Mat img, int thresh, bool saving) {
     // cv::morphologyEx(extracted, morphed2, cv::MORPH_OPEN, element,
     //                  cv::Point(-1, -1), 3);
 
-    cv::Mat inverted = invert(extracted);
+    inverted = invert(extracted);
 
     if (saving) {
-        save(inverted, path, "-extracted");
+        save(inverted, path, "-extracted-m6");
     }
     return inverted;
 }
