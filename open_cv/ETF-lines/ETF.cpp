f07cf52abc509efe6a433903fcfe05ea25567cd0
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-16 21:58:28 -0800

    Type checking

--- open_cv/ETF-lines/ETF.cpp
+++ open_cv/ETF-lines/ETF.cpp
@@ -1,1 +1,15 @@
 #include "ETF.hpp"
+
+cv::Mat ETF(std::string path, cv::Mat img, bool saving) {
+    cv ::Mat image;
+    image = read(path, img);
+    assert(!image.empty());
+    if (image.type() != 0) {
+        if (image.channels() == 4) {
+            cv::cvtColor(image, image, cv::COLOR_RGBA2GRAY);
+        } else if (image.channels() == 3) {
+            cv::cvtColor(image, image, cv::COLOR_RGB2GRAY);
+        }
+        image.convertTo(image, 0);
+    }
+}
