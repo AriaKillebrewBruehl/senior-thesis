0f2854b26f1083f8fdb0783a08e0a6070a067ef8
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-24 08:11:12 -0800

    Remove saves and prints

--- open_cv/edge-detection/extract-edges.cpp
+++ open_cv/edge-detection/extract-edges.cpp
@@ -31,14 +31,12 @@ cv::Mat extractEdges(std::string path, cv::Mat img, int thresh, bool saving) {
     // cv::Mat processed = processColors(src, nullptr);
 
     // assert(processed.type() == 0);
-    cv::Mat edges = cannyFilter(path, image, true);
+    cv::Mat edges = cannyFilter(path, image, false);
 
     // extract edges via threshold
     cv::Mat extracted = threshold(path, edges, thresh, false);
     cv::Mat inverted = invert(extracted);
-    save(inverted, path, "-extracted");
     extracted.convertTo(extracted, CV_8UC1);
-    std::cout << type2str(extracted.type()) << std::endl;
     cv::medianBlur(extracted, extracted, 5);
 
     // cv::Mat element =
