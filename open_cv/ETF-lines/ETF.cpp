a94ad969a51c4aaf24b7f226eb74859565a43cc7
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-25 16:48:59 -0800

    Add normalize funciton, treat T as separate X and Y matrices

--- open_cv/ETF-lines/ETF.cpp
+++ open_cv/ETF-lines/ETF.cpp
@@ -25,9 +25,11 @@ int phi(cv::Mat T, cv::Point2d x, cv::Point2d y) {
     return -1;
 }
 
-cv::Mat ETFFilter(cv::Mat tCur, int r, int u, int k) {
+cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, int r, int u, int k) {
     cv::Mat gNew = sobel_mag_angle("", tCur, false);
+    std::cout << "got g new" << std::endl;
     cv::Mat tNew = cv::Mat::zeros(tCur.size(), tCur.type());
+    std::cout << "got g new" << std::endl;
     for (int i = 0; i < tCur.rows; i++) {
         for (int j = 0; j < tCur.rows; j++) {
             float sum;
@@ -37,24 +39,40 @@ cv::Mat ETFFilter(cv::Mat tCur, int r, int u, int k) {
                     // since gHat is normalized we only need the direction
                     uchar gHat = gNew.at<cv::Vec2b>(i, j)[1];
                     int p = phi(tCur, cv::Point(i, j), cv::Point(y, x));
+                    std::cout << "got p" << std::endl;
                     int s = ws(cv::Point(i, j), cv::Point(y, x), r);
+                    std::cout << "got s" << std::endl;
                     float m = wm(cv::Point(i, j), cv::Point(y, x), u);
+                    std::cout << "got wm" << std::endl;
                     int d = wd(tCur, cv::Point(i, j), cv::Point(y, x));
+                    std::cout << "got wd" << std::endl;
                     sum += p * tCur.at<int>(y, x) * s * m * d;
-                    sum *= 1 / k;
-                    tNew.at<int>(i, j) = sum;
                 }
             }
+            sum *= 1 / k;
+            tNew.at<int>(i, j) = sum;
         }
     }
     return tNew;
 }
 
-cv::Vec2b perpendicular_normalize(cv::Vec2b g) {
-    cv::Vec2b t;
-    t[0] = g[0] + M_PI_2;
-    t[1] = 1;
-    return t;
+cv::Mat normalizeMatrix(cv::Mat m, int direction) {
+    assert(direction == 0 || direction == 1);
+    cv::Mat max, min;
+    cv::reduce(m, max, direction, cv::REDUCE_MAX);
+    cv::reduce(m, min, direction, cv::REDUCE_MIN);
+    uchar max_val = max.at<uchar>(0, 0);
+    uchar min_val = min.at<uchar>(0, 0);
+
+    cv::Mat normalized;
+    std::transform(m.begin<uchar>(), m.end<uchar>(), normalized.begin<uchar>(),
+                   [max_val, min_val](uchar p) -> uchar {
+                       p -= min_val;
+                       p /= (max_val - min_val);
+                       return p;
+                   });
+
+    return normalized;
 }
 
 cv::Mat ETF(std::string path, cv::Mat img, bool saving) {
@@ -71,24 +89,46 @@ cv::Mat ETF(std::string path, cv::Mat img, bool saving) {
     }
 
     // initial gradient map
-    // g0<cv::Vec2b>.at(i,j)[0] gives magnitude of the vector at (i, j)
-    // g0<cv::Vec2b>.at(i,j)[1] gives direction of the vector at (i, j)
-    cv::Mat g0 = sobel_mag_angle(path, image, false);
+    cv::Mat g0_merged = sobel_mag_angle(path, image, false);
+    // split into X and Y components
+    cv::Mat g0_channels[2];
+    cv::split(g0_merged, g0_channels);
+    cv::Mat g0X = g0_channels[0];
+    assert(g0X.type() == CV_8UC1);
+    cv::Mat g0Y = g0_channels[1];
+    assert(g0Y.type() == CV_8UC1);
 
+    // normalize by reducing values to [0, 1]
+    cv::Mat g0X_normalized = normalizeMatrix(g0X, 1);
+    cv::Mat g0Y_normalized = normalizeMatrix(g0Y, 0);
+
+    // initial t matrix is g0 rotated 90 degrees counter clockwise
+    cv::Mat t0X = g0Y_normalized * -1;
+    cv::Mat t0Y = g0X_normalized;
+
+    /*
     // calculate t0 by taking perpendicular vectors (CC) from g0
-    cv::Mat t0 = cv::Mat(g0.size(), g0.type());
-    std::transform(g0.begin<cv::Vec2b>(), g0.end<cv::Vec2b>(),
-                   t0.begin<cv::Vec2b>(), perpendicular_normalize);
 
     cv::Mat tCur = t0;
     for (int i = 0; i < 2; i++) {
         cv::Mat tNew = ETFFilter(tCur, 3, 1, 5);
+        std::cout << "here" << std::endl;
         tCur = tNew;
     }
 
-    if (saving) {
-        save(tCur, path, "-ETF");
+    cv::Mat visual = cv::Mat(tCur.size(), CV_8UC1);
+    for (int i = 0; i < tCur.rows; i++) {
+        for (int j = 0; j < tCur.cols; j++) {
+            visual.at<uchar>(i, j) = tCur.at<cv::Vec2b>(i, j)[1];
+        }
     }
 
-    return tCur;
+    i
+    */
+    if (saving) {
+        save(t0X, path, "-ETF-X");
+        save(t0Y, path, "-ETF-Y");
+    }
+
+    return image;
 }
