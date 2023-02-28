4ebd531db30f716e5c7c06d61bbcb26fe491254f
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-27 18:50:01 -0800

    Remove duplicate signatures

--- open_cv/ETF-lines/ETF.hpp
+++ open_cv/ETF-lines/ETF.hpp
@@ -18,16 +18,4 @@ cv::Mat1b normalizedGradientMagnitude(cv::Mat m);
 cv::Mat1b normalizeMatrix(cv::Mat m);
 cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, cv::Mat1b gHat, int r, int eta,
                   int nbrhood);
-cv::Mat ETF(std::string path, cv::Mat img, bool saving)
-
-    /*
-     cv::Mat normalizeMatrix(cv::Mat m, int direction)
-     Normalize a matrix representing the sobel operator
-     applied in either X or Y direction. Output matrix
-     will have values in the range [0, 1]. If m is
-     the sobel operator applied in the X direction use
-     `direction = 1`, use `directon = 0` for sobel operator
-     applied in the Y direciton.
-     */
-    cv::Mat normalizeMatrix(cv::Mat m, int direction);
 cv::Mat ETF(std::string path, cv::Mat img, bool saving);
