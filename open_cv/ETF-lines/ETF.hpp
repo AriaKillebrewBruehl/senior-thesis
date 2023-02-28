3089dcf2b2e58706278e3c34e7642dec72c73280
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-02-27 18:49:33 -0800

    Fix EFTFilter signature

--- open_cv/ETF-lines/ETF.hpp
+++ open_cv/ETF-lines/ETF.hpp
@@ -14,16 +14,20 @@ float g(cv::Point);
 float wm(cv::Point x, cv::Point y, int eta);
 int wd(cv::Vec2i x, cv::Vec2i y);
 int phi(cv::Point x, cv::Point y);
-cv::Mat ETFFilter(cv::Mat Tcur, int r, int u, int k);
+cv::Mat1b normalizedGradientMagnitude(cv::Mat m);
+cv::Mat1b normalizeMatrix(cv::Mat m);
+cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, cv::Mat1b gHat, int r, int eta,
+                  int nbrhood);
+cv::Mat ETF(std::string path, cv::Mat img, bool saving)
 
-/*
- cv::Mat normalizeMatrix(cv::Mat m, int direction)
- Normalize a matrix representing the sobel operator
- applied in either X or Y direction. Output matrix
- will have values in the range [0, 1]. If m is
- the sobel operator applied in the X direction use
- `direction = 1`, use `directon = 0` for sobel operator
- applied in the Y direciton.
- */
-cv::Mat normalizeMatrix(cv::Mat m, int direction);
+    /*
+     cv::Mat normalizeMatrix(cv::Mat m, int direction)
+     Normalize a matrix representing the sobel operator
+     applied in either X or Y direction. Output matrix
+     will have values in the range [0, 1]. If m is
+     the sobel operator applied in the X direction use
+     `direction = 1`, use `directon = 0` for sobel operator
+     applied in the Y direciton.
+     */
+    cv::Mat normalizeMatrix(cv::Mat m, int direction);
 cv::Mat ETF(std::string path, cv::Mat img, bool saving);
