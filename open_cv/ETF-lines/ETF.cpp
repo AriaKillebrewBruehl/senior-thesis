bede8798c552bd9a96beaa5b133b1879f03ab22f
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-01 07:37:57 -0800

    Change normalize functions to work with floats

--- open_cv/ETF-lines/ETF.cpp
+++ open_cv/ETF-lines/ETF.cpp
@@ -24,8 +24,8 @@ int phi(cv::Point x, cv::Point y) {
 
 cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, cv::Mat1b gHat, int r, int eta,
                   int nbrhood) {
-    // cv::Mat gNew = sobel_mag_angle("", tCur, false);
-    // std::cout << "got g new" << std::endl;
+    assert(tCurX.type() == CV_8SC1);
+    assert(tCurY.type() == CV_8SC1);
     cv::Mat tNewX = cv::Mat::zeros(tCurX.size(), tCurX.type());
     cv::Mat tNewY = cv::Mat::zeros(tCurY.size(), tCurY.type());
 
@@ -53,8 +53,8 @@ cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, cv::Mat1b gHat, int r, int eta,
                 }
             }
             sum *= 1 / k;
-            tNewX.at<int>(i, j) = sum[0];
-            tNewX.at<int>(i, j) = sum[1];
+            tNewX.at<uchar>(x) = sum[0];
+            tNewX.at<uchar>(x) = sum[1];
         }
     }
     cv::Mat channels[2] = {tNewX, tNewY};
@@ -63,23 +63,25 @@ cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, cv::Mat1b gHat, int r, int eta,
     return merged;
 }
 
-cv::Mat1b normalizedGradientMagnitude(cv::Mat m) {
+cv::Mat normalizedGradientMagnitude(cv::Mat m) {
     assert(m.channels() == 2);
-    cv::Mat magnitude = cv::Mat(m.size(), CV_8UC1);
+    cv::Mat magnitude = cv::Mat(m.size(), CV_32FC1);
     std::transform(m.begin<cv::Vec2b>(), m.end<cv::Vec2b>(),
-                   magnitude.begin<uchar>(), [](cv::Vec2b p) -> uchar {
+                   magnitude.begin<float32_t>(), [](cv::Vec2b p) -> float32_t {
                        uchar x = p[0];
                        uchar y = p[0];
-                       return uchar(sqrt(x * x + y * y));
+                       return float32_t(sqrt(x * x + y * y));
                    });
 
     cv::Mat1b normalized = normalizeMatrix(magnitude);
     return normalized;
 }
 
-cv::Mat1b normalizeMatrix(cv::Mat m) {
+cv::Mat normalizeMatrix(cv::Mat m) {
     assert(m.channels() == 1);
+    std::cout << "m type: " << type2str(m.type()) << std::endl;
     cv::Mat maxCols, max, minCols, min;
+    std::cout << "maxCols type: " << type2str(maxCols.type()) << std::endl;
     // reduce along columns
     cv::reduce(m, maxCols, 0, cv::REDUCE_MAX);
     cv::reduce(m, minCols, 0, cv::REDUCE_MIN);
@@ -90,15 +92,26 @@ cv::Mat1b normalizeMatrix(cv::Mat m) {
     uchar max_val = max.at<uchar>(0, 0);
     uchar min_val = min.at<uchar>(0, 0);
 
-    cv::Mat normalized = cv::Mat(m.size(), m.type());
-    std::transform(m.begin<uchar>(), m.end<uchar>(), normalized.begin<uchar>(),
-                   [max_val, min_val](uchar p) -> uchar {
-                       p -= min_val;
-                       p /= (max_val - min_val);
-                       return p;
-                   });
+    cv::Mat normalized = cv::Mat(m.size(), CV_32FC1);
+    std::transform(
+        m.begin<uchar>(), m.end<uchar>(), normalized.begin<float32_t>(),
+        [max_val, min_val](uchar p) -> float32_t {
+            p -= min_val;
+            float32_t n = float32_t(p) / float32_t(max_val - min_val);
+            return n;
+        });
 
+    std::cout << type2str(normalized.type()) << std::endl;
     return normalized;
+}
+
+cv::Mat scaleUpMatrix(cv::Mat m) {
+    assert(m.channels() == 1);
+    cv::Mat scaledUp = cv::Mat(m.size(), CV_8UC1);
+    std::transform(m.begin<schar>(), m.end<schar>(), scaledUp.begin<uchar>(),
+                   [](schar p) -> uchar { return abs(p) * 255; });
+
+    return scaledUp;
 }
 
 cv::Mat ETF(std::string path, cv::Mat img, bool saving) {
@@ -120,28 +133,54 @@ cv::Mat ETF(std::string path, cv::Mat img, bool saving) {
     cv::Mat g0_channels[2];
     cv::split(g0_merged, g0_channels);
     cv::Mat1b g0X = g0_channels[0];
-    assert(g0X.type() == CV_8UC1);
+    // save(g0X, path, "-g0X");
+    std::cout << "intitial g0X: \n" << g0X << std::endl;
     cv::Mat1b g0Y = g0_channels[1];
-    assert(g0Y.type() == CV_8UC1);
+    // save(g0Y, path, "-g0Y");
+    std::cout << "intitial g0Y: \n" << g0Y << std::endl;
 
     // normalize by reducing values to [0, 1]
-    cv::Mat1b g0X_normalized = normalizeMatrix(g0X);
-    cv::Mat1b g0Y_normalized = normalizeMatrix(g0Y);
+    cv::Mat g0X_normalized = normalizeMatrix(g0X);
+    std::cout << type2str(g0X_normalized.type()) << std::endl;
+    assert(g0X_normalized.type() == CV_8SC1);
+    // std::cout << "normalized g0X: \n" << g0X_normalized << std::endl;
+    cv::Mat g0Y_normalized = normalizeMatrix(g0Y);
+    assert(g0Y_normalized.type() == CV_8SC1);
+    // std::cout << "normalized g0Y: \n" << g0Y_normalized << std::endl;
+
+    // cv::Mat g0X_scaled = scaleUpMatrix(g0X_normalized);
+    // std::cout << "rescaled g0X: \n" << g0Y << std::endl;
+    // cv::Mat g0Y_scaled = scaleUpMatrix(g0Y_normalized);
+    // std::cout << "rescaled g0Y: \n" << g0Y << std::endl;
 
     // initial t0 matrix is the perpendicular (cc) vectors from the initial
     // gradient map g0
-    cv::Mat1b t0X = g0Y_normalized * -1;
-    cv::Mat1b t0Y = g0X_normalized;
+    cv::Mat t0X = g0Y_normalized * -1;
+    assert(t0X.type() == CV_8SC1);
+    cv::Mat t0Y = g0X_normalized;
+    assert(t0Y.type() == CV_8SC1);
+    std::string tagX = "-ETF-X-" + std::to_string(10);
+    std::string tagY = "-ETF-Y-" + std::to_string(10);
+    cv::Mat tCurXSacled = scaleUpMatrix(t0X);
+    cv::Mat tCurYSacled = scaleUpMatrix(t0Y);
+    save(tCurXSacled, path, tagX);
+    save(tCurXSacled, path, tagY);
 
     // compute normalized gradient magnitude of g0
-    cv::Mat1b gHat = normalizedGradientMagnitude(g0_merged);
-    std::cout << "called nGM" << std::endl;
+    cv::Mat gHat = normalizedGradientMagnitude(g0_merged);
+    assert(gHat.type() == CV_8UC1);
     cv::Mat tCurX = t0X;
     cv::Mat tCurY = t0Y;
+
     cv::Mat tNew;
     for (int i = 0; i < 2; i++) {
+        std::string tagX = "-ETF-X-" + std::to_string(i + 1);
+        std::string tagY = "-ETF-Y-" + std::to_string(i + 1);
+        cv::Mat tCurXSacled = scaleUpMatrix(tCurX);
+        cv::Mat tCurYSacled = scaleUpMatrix(tCurY);
+        save(tCurXSacled, path, tagX);
+        save(tCurXSacled, path, tagY);
         tNew = ETFFilter(tCurX, tCurY, gHat, 3, 1, 5);
-        std::cout << "called ETFF" << std::endl;
         cv::Mat t_channels[2];
         cv::split(tNew, t_channels);
         cv::Mat tCurX = t_channels[0];
