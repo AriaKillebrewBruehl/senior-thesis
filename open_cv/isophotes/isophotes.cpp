aad2705d846edb5a5a897de6ff81194dede1a7f1
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-06 19:06:28 -0800

    Change dictionary and heap to local variables, remove intermediate saving

--- open_cv/isophotes/isophotes.cpp
+++ open_cv/isophotes/isophotes.cpp
@@ -1,8 +1,5 @@
 #include "isophotes.hpp"
 
-std::unordered_map<uchar, int> colors{};
-std::priority_queue<color_freq, std::vector<color_freq>, comp> heap;
-
 cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
     // read in image
     cv::Mat image = read(path, img);
@@ -26,10 +23,11 @@ cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
         cv::bilateralFilter(src, dest, i, i * 2, i / 2);
         src = dest;
     }
-
     // convert image to CIE L*a*b
     cv::cvtColor(src, src, cv::COLOR_RGB2Lab);
 
+    std::unordered_map<uchar, int> colors{};
+    std::priority_queue<color_freq, std::vector<color_freq>, comp> heap;
     // luminance quantization and create color frequency map
     cv::Mat processed = processColors(src, &colors);
 
@@ -41,7 +39,6 @@ cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
     int s = (heap.size() <= thresh)
                 ? 1
                 : std::ceil(float(heap.size()) / float(thresh));
-
     uchar t;
     for (int i = 0; i < s; i++) {
         t = heap.top().first;
