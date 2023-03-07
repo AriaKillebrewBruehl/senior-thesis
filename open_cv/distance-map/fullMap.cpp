766a4c2efb5389b5d8c5ea75fef8b84b71001e3e
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-06 21:39:36 -0800

    Fix type conversion and remove hardcoded l value

--- open_cv/distance-map/fullMap.cpp
+++ open_cv/distance-map/fullMap.cpp
@@ -10,7 +10,9 @@ cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos,
     cv::Mat inverted_edges = invert(edges);
 
     if (inverted_edges.type() != 4) {
-        if (inverted_edges.channels() != 1) {
+        if (inverted_edges.channels() == 4) {
+            cv::cvtColor(inverted_edges, inverted_edges, cv::COLOR_RGBA2GRAY);
+        } else if (inverted_edges.channels() == 3) {
             cv::cvtColor(inverted_edges, inverted_edges, cv::COLOR_RGB2GRAY);
         }
         inverted_edges.convertTo(inverted_edges, 4);
@@ -23,14 +25,16 @@ cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos,
     cv::Mat inverted_isos = invert(isos);
 
     if (inverted_isos.type() != 4) {
-        if (inverted_isos.channels() != 1) {
+        if (inverted_isos.channels() == 4) {
+            cv::cvtColor(inverted_isos, inverted_isos, cv::COLOR_RGBA2GRAY);
+        } else if (inverted_isos.channels() == 3) {
             cv::cvtColor(inverted_isos, inverted_isos, cv::COLOR_RGB2GRAY);
         }
         inverted_isos.convertTo(inverted_isos, 4);
     }
 
     cv::Mat distances =
-        distanceMap("", inverted_edges, "", inverted_isos, false);
-    cv::Mat oMap = offsetMap(pathEdges, distances, 6.0, saving, false);
+        distanceMap(pathEdges, inverted_edges, pathIsos, inverted_isos, false);
+    cv::Mat oMap = offsetMap(pathEdges, distances, l, saving, offset_sections);
     return oMap;
 }
