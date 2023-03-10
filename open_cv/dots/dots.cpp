cbbc88bb20d5b7369ffca94e4e28cd83a85d0dbc
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-10 13:48:59 -0800

    Make dots just do adjusting

--- open_cv/dots/dots.cpp
+++ open_cv/dots/dots.cpp
@@ -1,7 +1,8 @@
 #include "dots.hpp"
 
-cv::Mat dots(std::string path, cv::Mat img, bool saving) {
-    cv::Mat offsets = read(path, img);
+cv::Mat dots(std::string pathOffset, cv::Mat imgOffset, std::string pathSeeds,
+             cv::Mat imgSeeds, int d, bool saving) {
+    cv::Mat offsets = read(pathOffset, imgOffset);
     assert(!offsets.empty());
 
     if (offsets.type() != 4) {
@@ -11,8 +12,15 @@ cv::Mat dots(std::string path, cv::Mat img, bool saving) {
         offsets.convertTo(offsets, 4);
     }
 
-    // step 0: generate seeds
-    cv::Mat seeds = placeSeeds("", offsets, 6, false);
+    cv::Mat seeds = read(pathSeeds, imgSeeds);
+    assert(!seeds.empty());
+
+    if (seeds.type() != 4) {
+        if (seeds.channels() != 1) {
+            cv::cvtColor(seeds, seeds, cv::COLOR_RGB2GRAY);
+        }
+        seeds.convertTo(seeds, 4);
+    }
 
     int t1 = 10;
     int t2 = 30;
@@ -25,7 +33,11 @@ cv::Mat dots(std::string path, cv::Mat img, bool saving) {
             // no offset lane constraints
             seeds = adjust("", offsets, "", dists, false, false);
             t1--;
-        } else {
+        }
+        if (t1 == 0 && t2 == 30) {
+            save(seeds, pathSeeds, "-after-t1");
+        }
+        if (!t1 && t2) {
             // alternate offset lane constraints
             seeds = adjust("", offsets, "", dists, false, t2 % 2);
             t2--;
@@ -33,7 +45,49 @@ cv::Mat dots(std::string path, cv::Mat img, bool saving) {
     }
 
     if (saving) {
-        save(seeds, path, "-adjusted");
+        save(seeds, pathSeeds, "-adjusted");
     }
     return seeds;
 }
+
+// cv::Mat dots(std::string path, cv::Mat img, int d, bool saving) {
+//     cv::Mat offsets = read(path, img);
+//     assert(!offsets.empty());
+
+//     if (offsets.type() != 4) {
+//         if (offsets.channels() != 1) {
+//             cv::cvtColor(offsets, offsets, cv::COLOR_RGB2GRAY);
+//         }
+//         offsets.convertTo(offsets, 4);
+//     }
+
+//     // step 0: generate seeds
+//     cv::Mat seeds = placeSeeds(path, offsets, 6, true);
+
+//     int t1 = 10;
+//     int t2 = 30;
+//     while (t1 || t2) {
+//         // step 1: generate distance map
+//         cv::Mat3i dists = sample_seeds(seeds, "", false, true);
+
+//         // step 2: adjust seed placement
+//         if (t1) {
+//             // no offset lane constraints
+//             seeds = adjust("", offsets, "", dists, false, false);
+//             t1--;
+//         }
+//         if (t1 == 0 && t2 == 30) {
+//             save(seeds, path, "-after-t1");
+//         }
+//         if (!t1 && t2) {
+//             // alternate offset lane constraints
+//             seeds = adjust("", offsets, "", dists, false, t2 % 2);
+//             t2--;
+//         }
+//     }
+
+//     if (saving) {
+//         save(seeds, path, "-adjusted");
+//     }
+//     return seeds;
+// }
