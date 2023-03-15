76a8dc44750318982c6199ed2c9d3db86ce388fc
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-15 15:43:14 -0700

    Remove colors in posterization

--- open_cv/isophotes/isophotes.cpp
+++ open_cv/isophotes/isophotes.cpp
@@ -1,6 +1,6 @@
 #include "isophotes.hpp"
 
-cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
+cv::Mat posterize(std::string path, cv::Mat img, int bins, bool saving) {
     // read in image
     cv::Mat image = read(path, img);
     assert(!image.empty());
@@ -25,12 +25,59 @@ cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
     }
     // convert image to CIE L*a*b
     cv::cvtColor(src, src, cv::COLOR_RGB2Lab);
+    // luminance quantization and create color frequency map
+    cv::Mat processed = processColors(src, bins);
+    if (saving) {
+        save(processed, path, "-posterized");
+    }
+
+    return processed;
+}
+
+cv::Mat getIsophotes(std::string path, cv::Mat img, int thresh, bool saving) {
+    // read in image
+    cv::Mat image = read(path, img);
+    assert(!image.empty());
+
+    if (image.type() != CV_8UC1) {
+        if (image.channels() == 3) {
+            cv::cvtColor(image, image, cv::COLOR_RGB2GRAY);
+        }
+        if (image.channels() == 4) {
+            cv::cvtColor(image, image, cv::COLOR_RGBA2GRAY);
+        }
+        image.convertTo(image, CV_8UC1);
+    }
+
+    // if (image.type() != 16) {
+    //     if (image.channels() == 1) {
+    //         cv::cvtColor(image, image, cv::COLOR_GRAY2RGB);
+    //     }
+    //     if (image.channels() == 4) {
+    //         cv::cvtColor(image, image, cv::COLOR_RGBA2RGB);
+    //     }
+    //     image.convertTo(image, 16);
+    // }
+
+    // int MAX_KERNEL_LENGTH = 15;
+    // cv::Mat src = image;
+    // // bilateral filter
+    // for (int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2) {
+    //     cv::Mat dest;
+    //     cv::bilateralFilter(src, dest, i, i * 2, i / 2);
+    //     src = dest;
+    // }
+    // // convert image to CIE L*a*b
+    // cv::cvtColor(src, src, cv::COLOR_RGB2Lab);
+
+    // std::unordered_map<uchar, int> colors{};
+    // std::priority_queue<color_freq, std::vector<color_freq>, comp> heap;
+    // // luminance quantization and create color frequency map
+    // cv::Mat processed = processColors(src, &colors, bins);
 
     std::unordered_map<uchar, int> colors{};
+    cv::Mat processed = posterize(path, image, &colors, bins, false);
     std::priority_queue<color_freq, std::vector<color_freq>, comp> heap;
-    // luminance quantization and create color frequency map
-    cv::Mat processed = processColors(src, &colors);
-
     // generate heap
     for (std::pair<uchar, int> i : colors) {
         heap.push(i);
