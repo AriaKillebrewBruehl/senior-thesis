edb9772ef199fe3999cf02a586b9c2d19ea79580
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-15 16:12:49 -0700

    Fix type checking in processColors

--- open_cv/boiler-plate/CIEprocess.cpp
+++ open_cv/boiler-plate/CIEprocess.cpp
@@ -1,13 +1,25 @@
 #include "CIEprocess.hpp"
 
 inline uchar reduceVal(const uchar val, int bins) {
-    return uchar(val / bins) * bins;
+    if (val < 255 - (255 / bins)) return uchar(val / bins + 5) * bins;
     // if (val < 192) return uchar(val / 64.0 + 0.5) * 64;
-    // return 255;
+    return 255;
 }
 
 // return a grayscale version of the image with only the L component
 cv::Mat processColors(cv::Mat img, int bins) {
+    assert(!img.empty());
+
+    if (img.type() != 16) {
+        if (img.channels() == 1) {
+            cv::cvtColor(img, img, cv::COLOR_GRAY2RGB);
+        }
+        if (img.channels() == 4) {
+            cv::cvtColor(img, img, cv::COLOR_RGBA2RGB);
+        }
+        img.convertTo(img, 16);
+    }
+
     cv::Mat gs = cv::Mat::zeros(img.rows, img.cols, CV_8UC1);
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
