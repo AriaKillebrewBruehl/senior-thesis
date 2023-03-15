1543ded3f2adadb180c103e2a94828b1ee3475eb
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-15 15:45:57 -0700

    Add bin parameter

--- open_cv/isophotes/extract-isophotes.hpp
+++ open_cv/isophotes/extract-isophotes.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <random>
 #include <string>
 #include <unordered_map>
-#include <random>
-#include <opencv2/opencv.hpp>
+
 #include "../boiler-plate/read-save.hpp"
 #include "../boiler-plate/type2str.hpp"
-#include "isophotes.hpp"
 #include "../edge-detection/extract-edges.hpp"
+#include "isophotes.hpp"
 
-cv::Mat extractIsophotes(std::string path, cv::Mat img, int edgethresh, int isothresh, bool saving);
+cv::Mat extractIsophotes(std::string path, cv::Mat img, int edgethresh,
+                         int isothresh, int bins, bool saving);
