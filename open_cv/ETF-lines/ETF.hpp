fba0c9bb3751a57b091a5c95d39557bb2df84f4d
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-15 11:22:17 -0700

    Add comments

--- open_cv/ETF-lines/ETF.hpp
+++ open_cv/ETF-lines/ETF.hpp
@@ -9,13 +9,15 @@
 #include "../boiler-plate/type2str.hpp"
 #include "sobel_operator.hpp"
 
+// spatial weight function
 int ws(cv::Vec2i x, cv::Vec2i y, int r);
 float g(cv::Point);
+// magnitude weight function
 float wm(cv::Point x, cv::Point y, cv::Mat gHat, int eta);
+// direction weight function
 int wd(cv::Vec2i x, cv::Vec2i y);
 int phi(cv::Point x, cv::Point y);
 cv::Mat normalizedGradientMagnitude(cv::Mat m);
 cv::Mat normalizeMatrix(cv::Mat m);
-cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, cv::Mat gHat, int r, int eta,
-                  int nbrhood);
+cv::Mat ETFFilter(cv::Mat tCurX, cv::Mat tCurY, cv::Mat gHat, int r, int eta);
 cv::Mat ETF(std::string path, cv::Mat img, bool saving);
