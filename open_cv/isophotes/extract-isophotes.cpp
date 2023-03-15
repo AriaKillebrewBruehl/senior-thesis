38d94c7cf42f398ba6493cca1d094e8b716af9a2
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-15 15:46:10 -0700

    Add bin parameter and call posterize

--- open_cv/isophotes/extract-isophotes.cpp
+++ open_cv/isophotes/extract-isophotes.cpp
@@ -1,7 +1,7 @@
 #include "extract-isophotes.hpp"
 
 cv::Mat extractIsophotes(std::string path, cv::Mat img, int edgethresh,
-                         int isothresh, bool saving) {
+                         int isothresh, int bins, bool saving) {
     cv::Mat image;
     image = read(path, img);
     assert(!image.empty());
@@ -16,8 +16,10 @@ cv::Mat extractIsophotes(std::string path, cv::Mat img, int edgethresh,
         image.convertTo(image, 16);
     }
 
+    // get posterized image
+    cv::Mat poster = posterize(path, image, bins, false);
     // get isophote image
-    cv::Mat isos = getIsophotes(path, image, isothresh, false);
+    cv::Mat isos = getIsophotes(path, poster, isothresh, false);
     assert(isos.type() == 0);
 
     // extract edges from isophote image
