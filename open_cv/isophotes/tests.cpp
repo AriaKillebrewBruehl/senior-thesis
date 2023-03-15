14556f10da27c8b98319a74b9643c03b37bbb7b1
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-15 11:21:37 -0700

    Formatting

--- open_cv/isophotes/tests.cpp
+++ open_cv/isophotes/tests.cpp
@@ -2,11 +2,12 @@
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-         std::cerr << "Must pass in image to run isophotes tests on." << std::endl;
+        std::cerr << "Must pass in image to run isophotes tests on."
+                  << std::endl;
     } else {
         for (int i = 1; i < argc; i++) {
             cv::Mat image;
-            extractIsophotes(argv[i],image, 150, 4, true);
+            extractIsophotes(argv[i], image, 150, 4, true);
         }
     }
 }
