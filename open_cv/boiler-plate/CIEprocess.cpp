c69dae55aaa83d384c3be2720ab7cb0847757039
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-23 10:40:39 -0700

    Remove custom binning

--- open_cv/boiler-plate/CIEprocess.cpp
+++ open_cv/boiler-plate/CIEprocess.cpp
@@ -1,17 +1,12 @@
 #include "CIEprocess.hpp"
 
-inline uchar reduceVal(const uchar val, int bins) {
-    int bin_size = 255 / bins;
-    // if (val > 255 - bin_size) {
-    //     return 255;
-    // } else {
-    return uchar(val / bin_size) * bin_size;
-    // if (val < 192) return uchar(val / 64.0 + 0.5) * 64;
-    // }
+inline uchar reduceVal(const uchar val) {
+    if (val < 192) return uchar(val / 64.0 + 0.5) * 64;
+    return 255;
 }
 
 // return a grayscale version of the image with only the L component
-cv::Mat processColors(cv::Mat img, int bins) {
+cv::Mat processColors(cv::Mat img) {
     assert(!img.empty());
 
     if (img.type() != 16) {
@@ -24,19 +19,10 @@ cv::Mat processColors(cv::Mat img, int bins) {
         img.convertTo(img, 16);
     }
 
-    // cv::Mat chans[3];
-    // cv::split(img, chans);
-    // cv::Mat L = chans[0];
-    // save(L, "", "-L");
-    // cv::Mat a = chans[1];
-    // save(a, "", "-a");
-    // cv::Mat b = chans[2];
-    // save(b, "", "-b");
-
     cv::Mat gs = cv::Mat::zeros(img.rows, img.cols, CV_8UC1);
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
-            uchar L = reduceVal(img.at<cv::Vec3b>(i, j)[0], bins);
+            uchar L = reduceVal(img.at<cv::Vec3b>(i, j)[0]);
             gs.at<uchar>(i, j) = (L);
         }
     }
