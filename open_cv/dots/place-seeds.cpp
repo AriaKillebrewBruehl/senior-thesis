6f8db410ab44db7a2164219f302d9bf5f93253e5
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-03-23 19:16:01 -0700

    Revert to default seed placement

--- open_cv/dots/place-seeds.cpp
+++ open_cv/dots/place-seeds.cpp
@@ -64,26 +64,21 @@ cv::Mat placeSeedsAdjusted(std::string pathOffset, cv::Mat offsetMap,
     // blank map for seed pixels
     cv::Mat seeds = cv::Mat(offset.rows, offset.cols, CV_8UC1, cv::Scalar(255));
     srand(time(0));
+
+    int total_seeds = 0;
     for (int i = 0; i < offset.rows; i++) {
         for (int j = 0; j < offset.cols; j++) {
             // feature lines are black
             if (offset.at<int32_t>(i, j) != 0) {
                 double r = ((double)rand()) / RAND_MAX;
-                int s;
-                int dist = distancess.at<int32_t>(i, j);
-                if (dist < 5) {
-                    s = d * 0.5;
-                } else if (dist < 10) {
-                    s = d * 0.75;
-                } else {
-                    s = d + (dist / 25);
-                }
-                if (r <= 1 / double(s * s)) {
+                if (r <= 1 / double(d * d)) {
                     seeds.at<uchar>(i, j) = uchar(0);
+                    total_seeds++;
                 }
             }
         }
     }
+    std::cout << "total seeds: " << total_seeds << std::endl;
 
     if (saving) {
         save(seeds, pathOffset, "-seeds");
