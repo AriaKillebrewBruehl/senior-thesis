03568e9b875740ed0696844554bfc5934e19c62d
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-04 12:42:55 -0700

    Read in details image

--- open_cv/distance-map/offset-map.cpp
+++ open_cv/distance-map/offset-map.cpp
@@ -1,7 +1,8 @@
 #include "offset-map.hpp"
 
-cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, float l, bool saving,
-                  bool sections) {
+cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists,
+                  std::string pathDetails, cv::Mat imgDetails, float l,
+                  bool saving, bool sections) {
     // read images and resize
     cv::Mat dists;
     dists = read(pathDists, imgDists);
@@ -11,6 +12,18 @@ cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, float l, bool saving,
             cv::cvtColor(dists, dists, cv::COLOR_RGB2GRAY);
         }
         dists.convertTo(dists, 0);
+    }
+
+    cv::Mat details;
+    details = read(pathDetails, imgDetails);
+    assert(!details.empty());
+    if (details.type() != 0) {
+        if (details.channels() == 3) {
+            cv::cvtColor(details, details, cv::COLOR_RGB2GRAY);
+        } else if (details.channels() == 4) {
+            cv::cvtColor(details, details, cv::COLOR_RGBA2GRAY);
+        }
+        details.convertTo(details, 0);
     }
 
     cv::Mat map = cv::Mat(dists.rows, dists.cols, CV_32SC1, cv::Scalar(255));
@@ -24,7 +37,7 @@ cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists, float l, bool saving,
             if (delta <= w) {
                 map.at<int32_t>(i, j) = 0;
             } else {
-                // if recording secitons give pixel id
+                // if recording sections give pixel id
                 // otherwise keep white
                 if (sections) {
                     map.at<int32_t>(i, j) = id + 1;
