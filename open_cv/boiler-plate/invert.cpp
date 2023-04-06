fb7a8da0d7f09a9b2780e57da88f20564ffb9ed9
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-05 19:33:22 -0700

    Fix type conversion and all white / black edge case

--- open_cv/boiler-plate/invert.cpp
+++ open_cv/boiler-plate/invert.cpp
@@ -3,10 +3,20 @@
 cv::Mat invert(cv::Mat arr) {
     assert(!arr.empty());
     if (arr.type() != 0) {
-        if (arr.channels() != 1) {
+        if (arr.channels() == 3) {
             cv::cvtColor(arr, arr, cv::COLOR_RGB2GRAY);
+        } else if (arr.channels() == 4) {
+            cv::cvtColor(arr, arr, cv::COLOR_RGBA2GRAY);
         }
         arr.convertTo(arr, 0);
+    }
+    if (cv::countNonZero(arr) == (arr.rows * arr.cols)) {
+        // image is fully white, so just return it
+        return arr;
+    } else if (cv::countNonZero(arr) == 0) {
+        // image is fully black, so invert and return
+        cv::bitwise_not(arr, arr);
+        return arr;
     }
 
     // get components
@@ -16,12 +26,11 @@ cv::Mat invert(cv::Mat arr) {
     int numComps =
         cv::connectedComponentsWithStats(arr, labels, stats, centroids);
 
-    int col = stats.at<int>(cv::CC_STAT_LEFT);
-    int row = stats.at<int>(cv::CC_STAT_TOP);
+    int col = stats.at<int>(0, cv::CC_STAT_LEFT);
+    int row = stats.at<int>(0, cv::CC_STAT_TOP);
     // background is black so invert
     if (arr.at<uchar>(row, col) == 0) {
         cv::bitwise_not(arr, arr);
     }
-
     return arr;
 }
