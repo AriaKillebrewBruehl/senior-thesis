0a539ebdac0fd7652063718c883eb18942d114bd
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-05 19:34:17 -0700

    Fix type conversion

--- open_cv/distance-map/offset-map.cpp
+++ open_cv/distance-map/offset-map.cpp
@@ -3,13 +3,16 @@
 cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists,
                   std::string pathDetails, cv::Mat imgDetails, float l,
                   bool saving, bool sections) {
+    std::cout << "in offset map" << std::endl;
     // read images and resize
     cv::Mat dists;
     dists = read(pathDists, imgDists);
     assert(!dists.empty());
     if (dists.type() != 0) {
-        if (dists.channels() != 1) {
+        if (dists.channels() == 3) {
             cv::cvtColor(dists, dists, cv::COLOR_RGB2GRAY);
+        } else if (dists.channels() == 4) {
+            cv::cvtColor(dists, dists, cv::COLOR_RGBA2GRAY);
         }
         dists.convertTo(dists, 0);
     }
@@ -28,6 +31,7 @@ cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists,
 
     cv::Mat map = cv::Mat(dists.size(), CV_32SC1, cv::Scalar(255));
     int w = 1;
+    std::cout << "about to fill in map" << std::endl;
     for (int i = 0; i < map.rows; i++) {
         for (int j = 0; j < map.cols; j++) {
             float d = float(dists.at<uchar>(i, j));
@@ -50,7 +54,7 @@ cv::Mat offsetMap(std::string pathDists, cv::Mat imgDists,
             }
         }
     }
-
+    std::cout << "filled map" << std::endl;
     if (saving) {
         save(map, pathDists, "-o-map-sections");
     }
