6c4f940d21ca129ce49345995202c42f60d805f5
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-08 17:56:03 -0700

    Includes and variables

--- open_cv/thingy-thangs/compare-gs.hpp
+++ open_cv/thingy-thangs/compare-gs.hpp
@@ -3,6 +3,17 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 
-#include "read-save.hpp"
+#include "../dots/dots.hpp"
+#include "../dots/place-dots.hpp"
+#include "../dots/place-seeds.hpp"
 
-void compare_gray(std::string path, cv::Mat image);
+int lane_size = 10;
+cv::Mat image;
+cv::Mat offset;
+cv::Mat details;
+cv::Mat seeds;
+cv::Mat adjusted_dots;
+cv::Mat stipples;
+
+void compare_gray(std::string path, cv::Mat img);
+#include "read-save.hpp"
