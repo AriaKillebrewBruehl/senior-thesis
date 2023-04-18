04aa612d3592e6979da9865e40bedd486e5b7269
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-18 12:06:57 -0700

    Add read-save

--- open_cv/thingy-thangs/compare-gs.hpp
+++ open_cv/thingy-thangs/compare-gs.hpp
@@ -6,14 +6,15 @@
 #include "../dots/dots.hpp"
 #include "../dots/place-dots.hpp"
 #include "../dots/place-seeds.hpp"
+#include "read-save.hpp"
 
-int lane_size = 10;
+int lane_size = 4;
 cv::Mat image;
 cv::Mat offset;
 cv::Mat details;
 cv::Mat seeds;
 cv::Mat adjusted_dots;
+int max_size;
 cv::Mat stipples;
 
-void compare_gray(std::string path, cv::Mat img);
-#include "read-save.hpp"
+float compare_gray(std::string path, cv::Mat img);
