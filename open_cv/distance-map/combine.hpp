35ad2323ac3b787e43e4ce5f991414302a093143
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-18 13:34:41 -0700

    Remove unused functions

--- open_cv/distance-map/combine.hpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string>
-#include <opencv2/opencv.hpp>
-#include "../boiler-plate/type2str.hpp"
-#include "../boiler-plate/read-save.hpp"
-
-cv::Mat combine(std::string pathEdge, cv::Mat imgEdge, std::string pathIsos, cv::Mat imgIsos, bool saving);
