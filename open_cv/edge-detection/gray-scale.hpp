cb286019930121dfb6eb93dfe9c64d5bf2fc42d7
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-19 11:49:35 -0700

    Remove unused files

--- open_cv/edge-detection/gray-scale.hpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string>
-#include <opencv2/opencv.hpp>
-#include "../boiler-plate/read-save.hpp"
-#include "../boiler-plate/type2str.hpp"
-
-cv::Mat grayscale(std::string path, cv::Mat img, bool saving) ;
