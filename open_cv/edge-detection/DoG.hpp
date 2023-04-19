596f923e91711b983085b8ccbf999bad77bb0327
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-19 11:23:59 -0700

    Remove unused files

--- open_cv/edge-detection/DoG.hpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <string>
-#include <opencv2/opencv.hpp>
-#include "../boiler-plate/type2str.hpp"
-#include "../boiler-plate/read-save.hpp"
-
-cv::Mat DoG(std::string path, cv::Mat img, bool saving);
