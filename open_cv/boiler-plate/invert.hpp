8c48bfb0d6a5b951ec899c4f94521f594676412f
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-19 13:16:18 -0700

    Add doc strings

--- open_cv/boiler-plate/invert.hpp
+++ open_cv/boiler-plate/invert.hpp
@@ -6,4 +6,9 @@
 #include "read-save.hpp"
 #include "type2str.hpp"
 
+// Returns an inverted image if the input image has a black background
+//
+// parameters:
+//
+// arr - the input image
 cv::Mat invert(cv::Mat arr);
