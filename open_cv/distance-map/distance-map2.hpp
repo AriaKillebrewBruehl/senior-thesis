151bbf0ad779e854c7a8ee6fcd1634ac6c08fd8d
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-19 11:56:44 -0700

    Add doc strings

--- open_cv/distance-map/distance-map2.hpp
+++ open_cv/distance-map/distance-map2.hpp
@@ -8,5 +8,20 @@
 #include "../boiler-plate/type2str.hpp"
 #include "sampled-transform-seeds.hpp"
 
+// parameters:
+//
+// pathEdges - the file path to the input edges
+// (can be "" if imgEdges is not empty)
+//
+// imgEdges - the edges as a cv::Mat
+// (can be empty if pathEdges is not "")
+//
+// pathIsos - the file path to the input isophotes
+// (can be "" if imgIsos is not empty)
+//
+// imgIsos - the isohpotes as a cv::Mat
+// (can be empty if pathIsos is not "")
+//
+// saving - saves the generated distance map to the file system
 cv::Mat distanceMap(std::string pathEdges, cv::Mat imgEdges,
                     std::string pathIsos, cv::Mat imgIsos, bool saving);
--- open_cv/distance-map/fullMap.hpp
+++ open_cv/distance-map/fullMap.hpp
@@ -1,18 +1,29 @@
 #pragma once
 #include "../boiler-plate/invert.hpp"
-#include "combine.hpp"
 #include "distance-map2.hpp"
 #include "offset-map.hpp"
 
-/*
-    cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string
-pathIsos, cv::Mat imgIsos, bool offset_sections, bool saving);
-
-Given an input image of edges and an
-input image of isophotes, generates a distance map from the edges and isophotes,
-then returns an offset map with a white background and offset lines of color
-corrsponding to the line's id if offset_sections is set, otherwise map is
-purely visual
-*/
+// Returns a distance map from the input edges and isophotes
+// parameters:
+// pathEdges - the path to the input edges
+// (can be "" is imgEdges is not empty)
+//
+// imgEdges - the input edges as a cv::Mat
+// (can be empty if pathEdges is not "")
+//
+// pathIsos - the path to the input isophotes
+// (can be "" is imgIsos is not empty)
+//
+// imgIsos - the input isophotes as a cv::Mat
+// (can be empty if pathIsos is not "")
+//
+// l - the integer distance between offset lines
+//
+// offset_sections - if true returns an offset map with
+// black offset lines and sections of color corresponding to the section id,
+// if false, returns an offset map with a white background and offset
+// lines of color corrsponding to the line's id
+//
+// saving - if true saves the generated offset map to the file system
 cv::Mat fullMap(std::string pathEdges, cv::Mat imgEdges, std::string pathIsos,
                 cv::Mat imgIsos, float l, bool offset_sections, bool saving);
