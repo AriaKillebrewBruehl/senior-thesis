5e868e19aa01c16cac306dc88cedc4d3a49e6c57
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-18 20:17:52 -0700

    Add doc strings

--- open_cv/edge-detection/extract-edges.hpp
+++ open_cv/edge-detection/extract-edges.hpp
@@ -11,4 +11,14 @@
 #include "canny.hpp"
 #include "threshold-edges.hpp"
 
+// Returns edges extracted from input image
+// parameters:
+//
+// path - file path to input image
+// (can be "" if img is not empty)
+//
+// img - the input image as a cv::Mat
+// (can be empty if path is not "")
+//
+// saving - saves edges to file system
 cv::Mat extractEdges(std::string path, cv::Mat img, int thresh, bool saving);
