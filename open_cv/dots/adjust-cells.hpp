7cb81792571c7fca1b32c934ac7bc8181a9ba1ce
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-19 13:23:27 -0700

    Add doc strings

--- open_cv/dots/adjust-cells.hpp
+++ open_cv/dots/adjust-cells.hpp
@@ -20,23 +20,33 @@ using pixel_type = std::pair<int, int>;
 using seed_map =
     std::unordered_map<pixel_type, std::vector<pixel_type>, hash_pair>;
 
-/*
+// Returns a seed map with the centroids adjusted
+// parameters:
+//
+// path_offset - the file path to the offset map with sections
+// (can be "" if img_offset is not empty)
+//
+// path_seeds - the file path to the seeds
+// (can be "" is img_seeds is not empty)
+//
+// img_offset - the offset map with sections as a cv::Mat
+// (can be empty if path_offset is not "")
+//
+// img_seeds - the seeds as a cv::Mat
+// (can be empty if path_seeds is not "")
+//
+// saving - save the adjusted seeds
+//
+// offset - offset line constraints will be considered
 cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
                std::string path_seeds, cv::Mat img_seeds, bool saving,
                bool offset);
 
-    Given an offset map that labels sections NOT a visual map or a map
-    labeling lines and a seed map, return a seed map with the centroids
-    adjusted. If offset is set offset line constraints will be considered.
-*/
-cv::Mat adjust(std::string path_offset, cv::Mat img_offset,
-               std::string path_seeds, cv::Mat img_seeds, bool saving,
-               bool offset);
-
-/*
-seed_map generate_map(cv::Mat image)
-Given a distance map return a map std::unordered_map<pixel_type,
-std::vector<pixel_type>> where map[(x,y)] = [(x',y')] means that pixel
-(x',y') is the the Voronoi cell with pixel (x,y) as its seed
-*/
+// generate_map returns a map of type
+//  std::unordered_map<pixel_type, std::vector<pixel_type>>
+// where map[(x,y)] = [(x',y')] means that pixel (x',y') is the the Voronoi cell
+// with pixel (x,y) as its seed
+// parameters:
+//
+// image - distance map as a cv::Mat
 seed_map generate_map(cv::Mat image);
--- open_cv/dots/place-seeds.hpp
+++ open_cv/dots/place-seeds.hpp
@@ -2,14 +2,42 @@
 
 #include "../boiler-plate/read-save.hpp"
 
-/*
-cv::Mat placeDots(std::string path, cv::Mat offsetMap, int d, bool saving)
-Given an offset map with sections labeled NOT a
-visual map or a map with lines labeled, randomly places seed pixels in black on
-a white image.
-*/
+// Returns a image with a random sampling of seed pixels that avoid offset lines
+//
+// parameters:
+//
+// path - the file path to the offset map
+// (can be "" if offsetMap is not empty)
+//
+// offsetMap - the offsetMap as a cv::Mat
+// (can be empty if path is not "")
+//
+// d - the approximate distance between seed pixels
+//
+// saving - saves the seeds to the file system
 cv::Mat placeSeeds(std::string path, cv::Mat offsetMap, std::string pathDetails,
                    cv::Mat imgDetails, int d, bool saving);
+
+// Returns a image with a random sampling of seed pixels that avoid offset lines
+// and consider distance from offset lines in seed placement
+//
+// parameters:
+//
+// pathOffset - the file path to the offset map
+// (can be "" if offsetMap is not empty)
+//
+// offsetMap - the offsetMap as a cv::Mat
+// (can be empty if pathOffset is not "")
+//
+// pathDists - the file path to the distance map
+// (can be "" if dists is not empty)
+//
+// dists - the distance map as a cv::Mat
+// (can be empty if pathDists is not "")
+//
+// d - the approximate distance between seed pixels
+//
+// saving - saves the seeds to the file system
 cv::Mat placeSeedsAdjusted(std::string pathOffset, cv::Mat offsetMap,
                            std::string pathDists, cv::Mat dists, int d,
                            bool saving);
