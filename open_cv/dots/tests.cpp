382bc1c1bbe613b1c5fbc0a69f4cdc3acd912a01
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-04-19 20:33:34 -0700

    comment out bad calls

--- open_cv/dots/tests.cpp
+++ open_cv/dots/tests.cpp
@@ -5,8 +5,7 @@ int main(int argc, char **argv) {
         std::cerr << "Must pass in image to run dot placement on." << std::endl;
     } else {
         for (int i = 1; i < argc; i += 2) {
-            cv::Mat image;
-            dots(argv[i], image, 6, true);
+            // cv::Mat image;
             // placeDots(argv[i], image, argv[i + 1], image, true);
         }
     }
