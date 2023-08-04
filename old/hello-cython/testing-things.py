49bad18348e4c0ad7a0695c9a3a6d1c6d917ef6c
AriaKillebrewBruehl <ariadkb@gmail.com>
2023-08-04 14:14:53 -0700

    Remove unused files

--- old/hello-cython/testing-things.py
+++
@@ -1,8 +0,0 @@
-#testing_things.py
-import timeit
-
-cy = timeit.timeit('''example_cython.test(5000)''',setup='import example_cython',number=100)
-py = timeit.timeit('''example_original.test(5000)''',setup='import example_original', number=100)
-
-print(cy, py)
-print('Cython is {}x faster'.format(py/cy))
--- open_cv/thingy-thangs/read-save.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include <opencv2/opencv.hpp>
-
-cv::Mat read(std::string path, cv::Mat img);
-void save(cv::Mat image, std::string path, std::string suffix);
